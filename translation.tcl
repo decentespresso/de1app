9168d380a88e23278216902062b6e979f77efd80
john <john>
2016-05-21 06:04:05 +0000

    *** empty log message ***

--- translation.tcl
+++ translation.tcl
@@ -1,6 +1,6 @@
 "ESPRESSO" 		{fr "ESPRESSO" de "ESPRESSO"}
 "HOT WATER" 	{fr "EAU CHAUDE" de "HEISSES WASSER"}
 "STEAM" 		{fr "VAPEUR" de "DAMPF"}
-"Cappuccino, Latte, Macchiato, Cortado, Flat White" {fr "Mousse, Caf\u00E9 cr\u00E8me, un noisette, cappuccino" de "Cappuccino, Latte, Macchiato, Cortado, Flat White"}
+"Cappuccino, Latte, Macchiato, Cortado, Flat White" {fr "Mousse, Caf\u00E9 cr\u00E8me, Noisette, Cappuccino" de "Cappuccino, Latte, Macchiato, Cortado, Flat White"}
 "Single, Double, Triple, Ristretto, Lungo" {fr "Serr\u00E9, Simple, Double, Allong\u00E9" de "Single, Double, Triple, Ristretto, Lungo"}
 "Americano, Tea" {fr "Americano, Th\u00E9" de "Americano, Tee"}
