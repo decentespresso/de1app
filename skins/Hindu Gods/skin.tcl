eca93c54c2a0b810571875a4c1f799d77ca01c5e
john <john>
2017-04-11 06:51:38 +0000

    no message

---
+++ skins/Hindu Gods/skin.tcl
@@ -0,0 +1,51 @@
+
+##############################################################################################################################################################################################################################################################################
+# DECENT ESPRESSO EXAMPLE SKIN FOR NEW SKIN DEVELOPERS
+##############################################################################################################################################################################################################################################################################
+
+# you should replace the JPG graphics in the 2560x1600/ directory with your own graphics. 
+source "[homedir]/skins/default/standard_includes.tcl"
+
+# example of loading a custom font (you need to indicate the TTF file and the font size)
+#load_font "Northwood High" "[skin_directory]/sample.ttf" 60
+#add_de1_text "off" 1280 500 -text "An important message" -font {Northwood High} -fill "#2d3046" -anchor "center"
+
+
+##############################################################################################################################################################################################################################################################################
+# text and buttons to display when the DE1 is idle
+
+load_font "Samarkan" "[skin_directory]/SAMAN.ttf" 38
+
+
+# these 3 text labels are for the three main DE1 functions, and they X,Y coordinates need to be adjusted for your skin graphics
+add_de1_text "off" 380 1455  -text [translate "ESPRESSO"] -font {Samarkan} -fill "#ffffff" -anchor "center" 
+add_de1_text "espresso" 1300 1350  -text [translate "ESPRESSO"] -font {Samarkan} -fill "#ffffff" -anchor "center" 
+
+add_de1_text "off" 1012 1455  -text [translate "STEAM"] -font {Samarkan} -fill "#ffffff" -anchor "center" 
+add_de1_text "steam" 1300 1450  -text [translate "STEAM"] -font {Samarkan} -fill "#ffffff" -anchor "center" 
+
+add_de1_text "off" 1585 1455 -text [translate "WATER"] -font {Samarkan} -fill "#ffffff" -anchor "center" 
+add_de1_text "water" 1300 1350 -text [translate "WATER"] -font {Samarkan} -fill "#ffffff" -anchor "center" 
+
+add_de1_text "off" 2186 1455  -text [translate "SETTINGS"] -font {Samarkan} -fill "#ffffff" -anchor "center" 
+add_de1_text "settings" 1300 1350  -text [translate "SETTINGS"] -font {Samarkan} -fill "#ffffff" -anchor "center" 
+
+
+
+# these 3 buttons are rectangular areas, where tapping the rectangle causes a major DE1 action (steam/espresso/water)
+add_de1_button "off" "say [translate {esspresso}] $::settings(sound_button_in);start_espresso" 70 520 635 1500
+add_de1_button "off" "say [translate {steam}] $::settings(sound_button_in);start_steam" 780 520 1230 1500
+add_de1_button "off" "say [translate {water}] $::settings(sound_button_in);start_water" 1350 520 1825 1500
+
+
+# these 2 buttons are rectangular areas for putting the machine to sleep or starting settings.  Traditionally, tapping one of the corners of the screen puts it to sleep.
+add_de1_button "off" "say [translate {sleep}] $::settings(sound_button_in);start_sleep" 125 80 950 330
+add_de1_button "off" {backup_settings; page_to_show_when_off settings_1} 1950 520 2425 1500
+
+##############################################################################################################################################################################################################################################################################
+
+# the standard behavior when the DE1 is doing something is for tapping anywhere on the screen to stop that. This "source" command does that.
+source "[homedir]/skins/default/standard_stop_buttons.tcl"
+
+
+
