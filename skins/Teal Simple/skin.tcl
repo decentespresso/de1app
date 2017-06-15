6c0d3a3826ecd50891805d2d866a294853054384
john <john>
2017-06-15 05:55:17 +0000

    no message

--- skins/Teal Simple/skin.tcl
+++ skins/Teal Simple/skin.tcl
@@ -20,21 +20,21 @@ load_font "novocento" "[skin_directory]/novocento.ttf" 28
 
 
 # these 3 text labels are for the three main DE1 functions, and they X,Y coordinates need to be adjusted for your skin graphics
-add_de1_text "off espresso" 450 1185  -text [translate "ESPRESSO"] -font {novocento} -fill "#ffffff" -anchor "center" 
-add_de1_text "off steam" 975 1185  -text [translate "STEAM"] -font {novocento} -fill "#ffffff" -anchor "center" 
-add_de1_text "off water" 1572 1185 -text [translate "WATER"] -font {novocento} -fill "#ffffff" -anchor "center" 
-add_de1_text "off settings" 2180 1185  -text [translate "SETTINGS"] -font {novocento} -fill "#ffffff" -anchor "center" 
+add_de1_text "off espresso" 450 1190  -text [translate "ESPRESSO"] -font {novocento} -fill "#ffffff" -anchor "center" 
+add_de1_text "off steam" 975 1190  -text [translate "STEAM"] -font {novocento} -fill "#ffffff" -anchor "center" 
+add_de1_text "off water" 1572 1190 -text [translate "WATER"] -font {novocento} -fill "#ffffff" -anchor "center" 
+add_de1_text "off settings" 2180 1190  -text [translate "SETTINGS"] -font {novocento} -fill "#ffffff" -anchor "center" 
 
 
 # these 3 buttons are rectangular areas, where tapping the rectangle causes a major DE1 action (steam/espresso/water)
-add_de1_button "off" "say [translate {espresso}] $::settings(sound_button_in);start_espresso" 250 700 650 1350
-add_de1_button "off" "say [translate {steam}] $::settings(sound_button_in);start_steam" 770 700 1150 1350
-add_de1_button "off" "say [translate {water}] $::settings(sound_button_in);start_water" 1400 700 1750 1350
+add_de1_button "off" "say [translate {espresso}] $::settings(sound_button_in);start_espresso" 250 800 650 1350
+add_de1_button "off" "say [translate {steam}] $::settings(sound_button_in);start_steam" 770 800 1150 1350
+add_de1_button "off" "say [translate {water}] $::settings(sound_button_in);start_water" 1400 800 1750 1350
 
 
 # these 2 buttons are rectangular areas for putting the machine to sleep or starting settings.  Traditionally, tapping one of the corners of the screen puts it to sleep.
-add_de1_button "off" "say [translate {sleep}] $::settings(sound_button_in);start_sleep" 850 75 1800 400
-add_de1_button "off" {backup_settings; page_to_show_when_off settings_1} 2000 700 2350 1350
+add_de1_button "off" "say [translate {sleep}] $::settings(sound_button_in);start_sleep" 850 300 1800 500
+add_de1_button "off" {backup_settings; page_to_show_when_off settings_1} 2000 800 2350 1350
 
 ##############################################################################################################################################################################################################################################################################
 
