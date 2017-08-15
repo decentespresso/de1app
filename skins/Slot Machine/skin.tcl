ba7b7574120294bab7c0c4d24c63ed0d13cbc3c2
john <john>
2017-08-15 09:03:29 +0000

    no message

--- skins/Slot Machine/skin.tcl
+++ skins/Slot Machine/skin.tcl
@@ -15,7 +15,7 @@ source "[homedir]/skins/default/standard_includes.tcl"
 ##############################################################################################################################################################################################################################################################################
 # text and buttons to display when the DE1 is idle
 
-load_font "renogare" "[skin_directory]/renogare.ttf" 28
+load_font "renogare" "[skin_directory]/renogare.ttf" 24
 
 
 # these 3 text labels are for the three main DE1 functions, and they X,Y coordinates need to be adjusted for your skin graphics
