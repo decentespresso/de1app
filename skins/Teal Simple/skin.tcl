e55357b0c3d0d33949374df374dd46f3448c98d8
john <john>
2017-08-15 10:37:23 +0000

    no message

--- skins/Teal Simple/skin.tcl
+++ skins/Teal Simple/skin.tcl
@@ -16,14 +16,14 @@ source "[homedir]/skins/default/standard_includes.tcl"
 # text and buttons to display when the DE1 is idle
 
 
-load_font "novocento" "[skin_directory]/novocento.ttf" 26
+load_font "novocento" "[skin_directory]/novocento.ttf" 24
 
 
 # these 3 text labels are for the three main DE1 functions, and they X,Y coordinates need to be adjusted for your skin graphics
-add_de1_text "off espresso" 450 1200  -text [translate "ESPRESSO"] -font {novocento} -fill "#ffffff" -anchor "center" 
-add_de1_text "off steam" 975 1200  -text [translate "STEAM"] -font {novocento} -fill "#ffffff" -anchor "center" 
-add_de1_text "off water" 1572 1200 -text [translate "WATER"] -font {novocento} -fill "#ffffff" -anchor "center" 
-add_de1_text "off settings" 2180 1200  -text [translate "SETTINGS"] -font {novocento} -fill "#ffffff" -anchor "center" 
+add_de1_text "off espresso" 450 1225  -text [translate "ESPRESSO"] -font {novocento} -fill "#ffffff" -anchor "center" 
+add_de1_text "off steam" 975 1225  -text [translate "STEAM"] -font {novocento} -fill "#ffffff" -anchor "center" 
+add_de1_text "off water" 1572 1225 -text [translate "WATER"] -font {novocento} -fill "#ffffff" -anchor "center" 
+add_de1_text "off settings" 2180 1225  -text [translate "SETTINGS"] -font {novocento} -fill "#ffffff" -anchor "center" 
 
 
 # these 3 buttons are rectangular areas, where tapping the rectangle causes a major DE1 action (steam/espresso/water)
@@ -33,7 +33,7 @@ add_de1_button "off" "say [translate {water}] $::settings(sound_button_in);start
 
 
 # these 2 buttons are rectangular areas for putting the machine to sleep or starting settings.  Traditionally, tapping one of the corners of the screen puts it to sleep.
-add_de1_button "off" "say [translate {sleep}] $::settings(sound_button_in);start_sleep" 850 250 1800 515
+add_de1_button "off" "say [translate {sleep}] $::settings(sound_button_in);start_sleep" 850 250 1800 555
 add_de1_button "off" {backup_settings; page_to_show_when_off settings_1} 2000 800 2350 1350
 
 ##############################################################################################################################################################################################################################################################################
