9849e4efda74c009aa7d7519199dafb4e1633829
john <john>
2017-09-08 10:09:21 +0000

    *** empty log message ***

--- de1plus-5.tcl
+++ de1plus-5.tcl
@@ -4,7 +4,9 @@ cd "[file dirname [info script]]/"
 source "pkgIndex.tcl"
 
 #set de1(has_flowmeter) 1
-set ::de1(de1_address) "EE:01:68:94:A5:48"
+#set ::de1(de1_address) "EE:01:68:94:A5:48"
+set ::de1(de1_address) "ed:85:72:b4:b3:a6"
+
 package provide de1plus 1.0
 package require de1_main
 de1_ui_startup
