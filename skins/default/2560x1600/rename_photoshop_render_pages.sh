72ae88bdba6c58ab573934732959ac014954f32c
john <john>
2017-09-28 15:15:26 +0000

    *** empty log message ***

---
+++ skins/default/2560x1600/rename_photoshop_render_pages.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# jpeg versions
+
+mv -f ~/Desktop/settings_pages_all0000.jpg settings_1.jpg
+mv -f ~/Desktop/settings_pages_all0001.jpg settings_2.jpg
+mv -f ~/Desktop/settings_pages_all0002.jpg settings_2a.jpg
+mv -f ~/Desktop/settings_pages_all0003.jpg settings_2b.jpg
+mv -f ~/Desktop/settings_pages_all0004.jpg settings_2c.jpg
+mv -f ~/Desktop/settings_pages_all0005.jpg settings_3.jpg
+mv -f ~/Desktop/settings_pages_all0006.jpg settings_4.jpg
+mv -f ~/Desktop/settings_pages_all0007.jpg settings_message.jpg
+
+# png vesions
+
+mv -f ~/Desktop/settings_pages_all0000.png settings_1.png
+mv -f ~/Desktop/settings_pages_all0001.png settings_2.png
+mv -f ~/Desktop/settings_pages_all0002.png settings_2a.png
+mv -f ~/Desktop/settings_pages_all0003.png settings_2b.png
+mv -f ~/Desktop/settings_pages_all0004.png settings_2c.png
+mv -f ~/Desktop/settings_pages_all0005.png settings_3.png
+mv -f ~/Desktop/settings_pages_all0006.png settings_4.png
+mv -f ~/Desktop/settings_pages_all0007.png settings_message.png
