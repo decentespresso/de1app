d124b90a90caa77b99b8d6ce52426c261db4d666
john <john>
2017-12-27 10:13:51 +0000

    no message

--- de1-2.tcl
+++
@@ -1,9 +0,0 @@
-#!/usr/local/bin/tclsh
-
-cd "[file dirname [info script]]/"
-source "pkgIndex.tcl"
-
-set de1(has_flowmeter) 0
-set ::de1(de1_address) "C5:80:EC:A5:F9:72"
-package require de1_main
-de1_ui_startup
--- de1-5.tcl
+++
@@ -1,9 +0,0 @@
-#!/usr/local/bin/tclsh
-
-cd "[file dirname [info script]]/"
-source "pkgIndex.tcl"
-set de1(has_flowmeter) 0
-#set ::de1(de1_address) "EE:01:68:94:A5:48"
-set ::de1(de1_address) "ed:85:72:b4:b3:a6"
-package require de1_main
-de1_ui_startup
