e86cebd1b513de23e8afb9fc7afa44d55fd016c2
john <john>
2019-11-07 00:29:38 +0000

    *** empty log message ***

--- bluetooth.tcl
+++ bluetooth.tcl
@@ -1363,15 +1363,19 @@ proc de1_ble_handler { event data } {
 					    	#ble_connect_to_scale
 					    }
 
-				    	set do_this 1
-				    	if {$do_this == 1} {
-							catch {
-						    	ble close $::currently_connecting_scale_handle
-						    }
+						catch {
+					    	ble close $::currently_connecting_scale_handle
+					    }
 
-						    set ::currently_connecting_scale_handle 0
+					    set ::currently_connecting_scale_handle 0
+			    		
+					    # john 1-11-19 automatic reconnection attempts eventually kill the bluetooth stack on android 5.1
+					    # john might want to make this happen automatically on Android 8, though. For now, it's a setting, which might 
+					    # eventually get auto-set as per the current Android version, if we can trust that to give us a reliable BLE stack.
+						if {$::settings(automatically_ble_reconnect_forever_to_scale) != 1} {
 				    		ble_connect_to_scale
 				    	}
+
 				    }
 				} elseif {$state eq "scanning"} {
 					set ::scanning 1
@@ -1718,7 +1722,7 @@ proc de1_ble_handler { event data } {
 								return
 							}
 
-							msg "sensorweight: $sensorweight"
+							#msg "sensorweight: $sensorweight"
 
 							if {$sensorweight < 0 && $::de1_num_state($::de1(state)) == "Idle"} {
 
