6a5789083bc46823db1b7380c9d2b871a2819f06
john <john>
2019-11-11 16:49:31 +0000

    *** empty log message ***

--- updater.tcl
+++ updater.tcl
@@ -410,6 +410,7 @@ proc start_app_update {} {
     ##############################################################################################################
     # get manifest both as raw TXT and as gzip compressed, to detect tampering 
     set url_manifest "$host/download/sync/$progname/manifest.txt"
+    set url_manifest "$host/download/sync/$progname/manifest.tdb"
     set remote_manifest {}
     catch {
         set remote_manifest [string trim [decent_http_get $url_manifest]]
