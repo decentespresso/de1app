0d8f9fb84f2adced57d011224a73f19834fea941
john <john>
2020-04-08 18:14:11 +0000

    *** empty log message ***

--- misc.tcl
+++ misc.tcl
@@ -91,8 +91,8 @@ proc make_de1_dir {} {
         fonts/notosansuiregular.ttf *
         fonts/sarabun.ttf *
         fonts/sarabunbold.ttf *
-        fonts/agamashq.ttf *
-        fonts/agamashq_bold.ttf *
+        fonts/Dubai-Bold.otf *
+        fonts/Dubai-Regular *
 
         godshots/none.shot *
         godshots/import/readme.txt *
