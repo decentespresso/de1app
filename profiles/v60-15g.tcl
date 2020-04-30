7a5dcea2f0645140df211acf861f7760eaa2b0a9
john <john>
2020-04-30 20:35:59 +0000

    *** empty log message ***

--- profiles/v60-15g.tcl
+++ profiles/v60-15g.tcl
@@ -1,4 +1,4 @@
-advanced_shot {{exit_if 0 flow 8 volume 100 transition fast exit_flow_under 0 temperature 100 name preinfusion pressure 1 sensor coffee pump flow exit_type pressure_over exit_flow_over 6 exit_pressure_over 1.5 exit_pressure_under 0 seconds 10.0} {exit_if 0 flow 0 volume 100 transition fast exit_flow_under 0 temperature 100 name bloom pressure 8.6 sensor coffee pump flow exit_flow_over 6 exit_pressure_over 11 seconds 30.0 exit_pressure_under 0} {exit_if 0 flow 8.0 volume 100 transition fast exit_flow_under 0 temperature 100 name pulse pressure 6.0 sensor coffee pump flow exit_flow_over 6 exit_pressure_over 11 seconds 9.0 exit_pressure_under 0} {exit_if 0 flow 0.0 volume 100 transition fast exit_flow_under 0 temperature 100 name pause pressure 6.0 sensor coffee pump flow exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 30.0} {exit_if 0 flow 8 volume 100 transition fast exit_flow_under 0 temperature 100 name pulse pressure 6.0 sensor coffee pump flow exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 8.0} {exit_if 0 flow 0.0 volume 100 transition fast exit_flow_under 0 temperature 100 name pause pressure 6.0 sensor coffee pump flow exit_flow_over 6 exit_pressure_over 11 seconds 30.0 exit_pressure_under 0} {exit_if 0 flow 8.0 volume 100 transition fast exit_flow_under 0 temperature 100 name pulse pressure 6.0 sensor coffee pump flow exit_flow_over 6 exit_pressure_over 11 seconds 8.0 exit_pressure_under 0}}
+advanced_shot {{exit_if 0 flow 8 volume 100 transition fast exit_flow_under 0 temperature 100 name preinfusion pressure 1 sensor coffee pump flow exit_type pressure_over exit_flow_over 6 exit_pressure_over 1.5 seconds 11.00 exit_pressure_under 0} {exit_if 0 flow 0 volume 100 transition fast exit_flow_under 0 temperature 100 name bloom pressure 0 sensor coffee pump pressure exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 30.0} {exit_if 0 flow 8.0 volume 100 transition fast exit_flow_under 0 temperature 100 name pulse pressure 6.0 sensor coffee pump flow exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 10.00} {exit_if 0 flow 0.0 volume 100 transition fast exit_flow_under 0 temperature 100 name pause pressure 0 sensor coffee pump pressure exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 30.0} {exit_if 0 flow 8 volume 100 transition fast exit_flow_under 0 temperature 100 name pulse pressure 6.0 sensor coffee pump flow exit_flow_over 6 exit_pressure_over 11 seconds 9.00 exit_pressure_under 0} {exit_if 0 flow 0.0 volume 100 transition fast exit_flow_under 0 temperature 100 name pause pressure 0.00 sensor coffee pump pressure exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 30.0} {exit_if 0 flow 8.0 volume 100 transition fast exit_flow_under 0 temperature 100 name pulse pressure 6.0 sensor coffee pump flow exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 9.00}}
 author Decent
 espresso_decline_time 25
 espresso_hold_time 10
@@ -22,9 +22,9 @@ preinfusion_time 20
 pressure_end 6.0
 profile_hide 1
 profile_language en
-profile_notes {Scott Rao's 15g profile for pouur overs using the Decent pour over basket.}
+profile_notes {Scott Rao's 15g profile for pour overs using the Decent pour over basket.}
 profile_title {V60 15g}
 settings_profile_type settings_2c
-tank_desired_water_temperature 0
+tank_desired_water_temperature 30
 water_temperature 80
 
--- profiles/v60-20g.tcl
+++ profiles/v60-20g.tcl
@@ -1,9 +1,9 @@
-advanced_shot {{exit_if 0 flow 8 volume 100 transition fast exit_flow_under 0 temperature 100 name preinfusion pressure 1 sensor coffee pump flow exit_type pressure_over exit_flow_over 6 exit_pressure_over 1.5 exit_pressure_under 0 seconds 12.00} {exit_if 0 flow 0.0 volume 100 transition fast exit_flow_under 0 temperature 100 name bloom* pressure 8.6 sensor coffee pump flow exit_flow_over 6 exit_pressure_over 11 seconds 30.00 exit_pressure_under 0} {exit_if 0 flow 8.0 volume 100 transition fast exit_flow_under 0 temperature 100 name pulse pressure 6.0 sensor coffee pump flow exit_flow_over 6 exit_pressure_over 11 seconds 11.00 exit_pressure_under 0} {exit_if 0 flow 0.0 volume 100 transition fast exit_flow_under 0 temperature 100 name pause pressure 6.0 sensor coffee pump flow exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 35.00} {exit_if 0 flow 8 volume 100 transition fast exit_flow_under 0 temperature 100 name pulse pressure 6.0 sensor coffee pump flow exit_flow_over 6 exit_pressure_over 11 seconds 11.00 exit_pressure_under 0} {exit_if 0 flow 0.0 volume 100 transition fast exit_flow_under 0 temperature 100 name pause pressure 6.0 sensor coffee pump flow exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 35.00} {exit_if 0 flow 8.0 volume 100 transition fast exit_flow_under 0 temperature 100 name pulse pressure 6.0 sensor coffee pump flow exit_flow_over 6 exit_pressure_over 11 seconds 11.00 exit_pressure_under 0} {exit_if 0 flow 0.1 volume 100 transition fast exit_flow_under 0 temperature 100 name {[ spin at end]*} pressure 6.0 sensor coffee pump flow exit_flow_over 6 exit_pressure_over 11 seconds 1 exit_pressure_under 0}}
+advanced_shot {{exit_if 0 flow 8 volume 100 transition fast exit_flow_under 0 temperature 100 name preinfusion pressure 1 sensor coffee pump flow exit_type pressure_over exit_flow_over 6 exit_pressure_over 1.5 seconds 13.00 exit_pressure_under 0} {exit_if 0 flow 0.0 volume 100 transition fast exit_flow_under 0 temperature 100 name bloom* pressure 0 sensor coffee pump pressure exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 30.00} {exit_if 0 flow 8.0 volume 100 transition fast exit_flow_under 0 temperature 100 name pulse pressure 6.0 sensor coffee pump flow exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 12.00} {exit_if 0 flow 0.0 volume 100 transition fast exit_flow_under 0 temperature 100 name pause pressure 0.00 sensor coffee pump pressure exit_flow_over 6 exit_pressure_over 11 seconds 35.00 exit_pressure_under 0} {exit_if 0 flow 8 volume 100 transition fast exit_flow_under 0 temperature 100 name pulse pressure 6.0 sensor coffee pump flow exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 12.00} {exit_if 0 flow 0.0 volume 100 transition fast exit_flow_under 0 temperature 100 name pause pressure 0 sensor coffee pump pressure exit_flow_over 6 exit_pressure_over 11 seconds 35.00 exit_pressure_under 0} {exit_if 0 flow 8.0 volume 100 transition fast exit_flow_under 0 temperature 100 name pulse pressure 6.0 sensor coffee pump flow exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 12.00} {exit_if 0 flow 0.1 volume 100 transition fast exit_flow_under 0 temperature 100 name {[ spin at end]*} pressure 6.0 sensor coffee pump flow exit_flow_over 6 exit_pressure_over 11 seconds 1 exit_pressure_under 0}}
 author Decent
 espresso_decline_time 25
 espresso_hold_time 10
 espresso_pressure 8.6
-espresso_temperature 100
+espresso_temperature 100.0
 final_desired_shot_volume 32
 final_desired_shot_volume_advanced 0
 final_desired_shot_weight 32
@@ -25,6 +25,6 @@ profile_language en
 profile_notes {Scott Rao's 20g profile for pour overs using the Decent pour over basket.}
 profile_title {V60 20g}
 settings_profile_type settings_2c
-tank_desired_water_temperature 28
+tank_desired_water_temperature 60
 water_temperature 80
 
