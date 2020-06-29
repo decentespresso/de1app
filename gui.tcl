ae7191c7bf487bb0a02117fe89efbfeef2cea7f0
john <john>
2020-06-29 20:12:18 +0000

    *** empty log message ***

--- gui.tcl
+++ gui.tcl
@@ -920,7 +920,7 @@ proc show_going_to_sleep_page  {} {
 		return
 	}
 
-    if {$::de1(currently_updating_firmware) == 1} {
+	if {$::de1(currently_updating_firmware) == 1 || [ifexists ::de1(in_fw_update_mode)] == 1} {
 		msg "delaying screen saver because firmware is updating"
 		delay_screen_saver
 		return
