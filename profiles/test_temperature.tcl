ffaf671b2ba2fc482fc081c36c3d737e331a2d52
john <john>
2020-08-15 03:57:33 +0000

    *** empty log message ***

--- profiles/test_temperature.tcl
+++ profiles/test_temperature.tcl
@@ -1,4 +1,4 @@
-advanced_shot {{exit_if 0 flow 1.5 volume 100 transition fast exit_flow_under 0 temperature 70.00 name 70c pressure 9.00 sensor coffee pump pressure exit_type pressure_over exit_flow_over 6 exit_pressure_over 10.0 exit_pressure_under 0 seconds 60.00} {exit_if 0 flow 1.5 volume 100 transition fast exit_flow_under 0 temperature 80.00 name 80c pressure 9.00 sensor coffee pump pressure exit_type pressure_over exit_flow_over 6 exit_pressure_over 10.0 exit_pressure_under 0 seconds 30.00} {exit_if 0 flow 1.5 volume 100 transition fast exit_flow_under 0 temperature 90.00 name 90c pressure 9.00 sensor coffee pump pressure exit_type pressure_over exit_flow_over 6 exit_pressure_over 10.0 exit_pressure_under 0 seconds 30.00} {exit_if 0 flow 1.5 volume 100 transition fast exit_flow_under 0 temperature 98.00 name 98c pressure 9.00 sensor coffee pump pressure exit_type pressure_over exit_flow_over 6 exit_pressure_over 10.0 exit_pressure_under 0 seconds 30.00}}
+advanced_shot {{exit_if 0 flow 4.00 volume 100 transition fast exit_flow_under 0 temperature 70.00 name 70c pressure 9.00 sensor coffee pump flow exit_type pressure_over exit_flow_over 6 exit_pressure_over 10.0 seconds 20.00 exit_pressure_under 0} {exit_if 0 flow 4.00 volume 100 transition fast exit_flow_under 0 temperature 80.00 name 80c pressure 9.00 sensor coffee pump flow exit_type pressure_over exit_flow_over 6 exit_pressure_over 10.0 exit_pressure_under 0 seconds 40.00} {exit_if 0 flow 4.00 volume 100 transition fast exit_flow_under 0 temperature 90.00 name 90c pressure 9.00 sensor coffee pump flow exit_type pressure_over exit_flow_over 6 exit_pressure_over 10.0 exit_pressure_under 0 seconds 40.00} {exit_if 0 flow 4.00 volume 100 transition fast exit_flow_under 0 temperature 100.00 name 100c pressure 9.00 sensor coffee pump flow exit_type pressure_over exit_flow_over 6 exit_pressure_over 10.0 exit_pressure_under 0 seconds 40.00}}
 espresso_temperature_steps_enabled 1
 author Decent
 espresso_hold_time 30
@@ -11,7 +11,7 @@ espresso_temperature_0 0.0
 espresso_temperature_1 0.0
 espresso_temperature_2 0.0
 espresso_temperature_3 0.0
-settings_profile_type settings_2c
+settings_profile_type settings_2c2
 flow_profile_preinfusion 4
 flow_profile_preinfusion_time 5
 flow_profile_hold 2
@@ -20,7 +20,7 @@ flow_profile_decline 1.2
 flow_profile_decline_time 17
 flow_profile_minimum_pressure 4
 preinfusion_flow_rate 1.5
-profile_notes {Put a 0.3mm puck simulator basket in your DE1.  Look for smooth temperature ramps between each temperature stage.  Also look for a flow rate around 1.6 ml/s with this basket.}
+profile_notes {Put a pour over basket in your DE1.  Look for smooth temperature ramps between each temperature stage.  }
 water_temperature 70.00
 final_desired_shot_volume 0
 final_desired_shot_weight 32
@@ -32,5 +32,5 @@ profile_title {Test temperature}
 profile_language en
 preinfusion_stop_pressure 10.0
 profile_hide 0
-final_desired_shot_volume_advanced_count_start 2
+final_desired_shot_volume_advanced_count_start 0
 
