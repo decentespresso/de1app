26c684226d7b0e615901fd85a09be4e5b161a719
john <john>
2020-09-01 04:02:25 +0000

    *** empty log message ***

--- profiles/tea_portafilter.tcl
+++ profiles/tea_portafilter.tcl
@@ -1,4 +1,4 @@
-advanced_shot {{exit_if 1 flow 6.00 volume 100 transition fast exit_flow_under 0 temperature 100 name Fill pressure 1 sensor coffee pump flow exit_type pressure_over exit_flow_over 6 exit_pressure_over 2.00 seconds 20.00 exit_pressure_under 0} {exit_if 0 flow 4 volume 100 transition fast exit_flow_under 0 temperature 100 name Infuse pressure 0.50 sensor coffee pump pressure exit_type pressure_over exit_flow_over 6 exit_pressure_over 2.00 exit_pressure_under 0 seconds 30.00} {exit_if 0 flow 6.00 volume 100 transition fast exit_flow_under 0 temperature 100 name Flush pressure 6.00 sensor coffee pump flow exit_type pressure_over exit_flow_over 6 exit_pressure_over 2.00 exit_pressure_under 0 seconds 12.00} {exit_if 0 flow 4.00 volume 100 transition fast exit_flow_under 0 temperature 100 name Infuse pressure 0.50 sensor coffee pump pressure exit_type pressure_over exit_flow_over 6 exit_pressure_over 2.00 exit_pressure_under 0 seconds 40.00} {exit_if 0 flow 6.00 volume 100 transition fast exit_flow_under 0 temperature 100 name Flush pressure 0.50 sensor coffee pump flow exit_type pressure_over exit_flow_over 6 exit_pressure_over 2.00 exit_pressure_under 0 seconds 12.00} {exit_if 0 flow 6.00 volume 100 transition fast exit_flow_under 0 temperature 100 name Infuse pressure 0.50 sensor coffee pump pressure exit_type pressure_over exit_flow_over 6 exit_pressure_over 2.00 exit_pressure_under 0 seconds 40.00} {exit_if 0 flow 6.00 volume 100 transition fast exit_flow_under 0 temperature 100 name Flush pressure 0.50 sensor coffee pump flow exit_type pressure_over exit_flow_over 6 exit_pressure_over 2.00 exit_pressure_under 0 seconds 12.00}}
+advanced_shot {{exit_if 1 flow 6.00 volume 100 transition fast exit_flow_under 0 temperature 100 name Fill pressure 1 sensor coffee pump flow exit_type pressure_over exit_flow_over 6 exit_pressure_over 2.00 seconds 20.00 exit_pressure_under 0} {exit_if 0 flow 4 volume 100 transition fast exit_flow_under 0 temperature 100 name Infuse pressure 0.50 sensor coffee pump pressure exit_type pressure_over exit_flow_over 6 exit_pressure_over 2.00 exit_pressure_under 0 seconds 30.00} {exit_if 0 flow 6.00 volume 100 transition fast exit_flow_under 0 temperature 100 name Flush pressure 6.00 sensor coffee pump flow exit_type pressure_over exit_flow_over 6 exit_pressure_over 2.00 seconds 15.00 exit_pressure_under 0} {exit_if 0 flow 4.00 volume 100 transition fast exit_flow_under 0 temperature 100 name Infuse pressure 0.50 sensor coffee pump pressure exit_type pressure_over exit_flow_over 6 exit_pressure_over 2.00 exit_pressure_under 0 seconds 40.00} {exit_if 0 flow 6.00 volume 100 transition fast exit_flow_under 0 temperature 100 name Flush pressure 0.50 sensor coffee pump flow exit_type pressure_over exit_flow_over 6 exit_pressure_over 2.00 seconds 15.00 exit_pressure_under 0} {exit_if 0 flow 6.00 volume 100 transition fast exit_flow_under 0 temperature 100 name Infuse pressure 0.50 sensor coffee pump pressure exit_type pressure_over exit_flow_over 6 exit_pressure_over 2.00 exit_pressure_under 0 seconds 40.00} {exit_if 0 flow 6.00 volume 100 transition fast exit_flow_under 0 temperature 100 name Flush pressure 0.50 sensor coffee pump flow exit_type pressure_over exit_flow_over 6 exit_pressure_over 2.00 seconds 15.00 exit_pressure_under 0}}
 author Decent
 espresso_decline_time 0
 espresso_hold_time 25
@@ -10,8 +10,8 @@ espresso_temperature_2 88.0
 espresso_temperature_3 88.0
 espresso_temperature_steps_enabled 1
 final_desired_shot_volume 36
-final_desired_shot_volume_advanced 36
-final_desired_shot_volume_advanced_count_start 2
+final_desired_shot_volume_advanced 0
+final_desired_shot_volume_advanced_count_start 1
 final_desired_shot_weight 36
 final_desired_shot_weight_advanced 36
 flow_profile_decline 1.2
@@ -30,7 +30,7 @@ profile_hide 1
 profile_language en
 profile_notes {This profile requires a specially designed tea portafilter that opens its valve at pressures of 3 bar or above.  Put a tea bag or loose leaf tea into the basket.  Makes about 250ml of tea in 3 minutes.}
 profile_title {Tea portafilter}
-settings_profile_type settings_2c
+settings_profile_type settings_2c2
 tank_desired_water_temperature 0
 water_temperature 80
 
