a4efbdd5c78acc50bf0733ae1106343f07aa345c
Mimoja <git@mimoja.de>
2020-10-22 11:19:43 +0200

    misc: Move version to version.tcl
    To be able to change the version easily programmatically we are moving the version
    definition to its own minimal version.tcl file.
    So far the version was defined in the de1plus.tcl where there are many things that could
    break when changed automatically.
    
    Signed-off-by: Mimoja <git@mimoja.de>

--- de1plus/de1plus.tcl
+++ de1plus/de1plus.tcl
@@ -3,7 +3,7 @@
 encoding system utf-8
 cd "[file dirname [info script]]/"
 source "pkgIndex.tcl"
-package ifneeded de1app 1.32 {}
+source "version.tcl"
 package provide de1plus 1.0
 package require de1_main
 de1_ui_startup
