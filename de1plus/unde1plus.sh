840ac11188746182cbcc95dd9234175d6d3a556e
John Buckman <john@decentespresso.com>
2020-10-28 17:59:48 +0800

    new unde1plus shell script for running the de1app from inside undroidwish, during development, on your own OS desktop

---
+++ de1plus/unde1plus.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# You should put undroidwish into your path to have this script work
+# 
+# On Mac OSX, you can for example do this this:
+# 
+# sudo ln -s /Applications/undroidwish.app/Contents/MacOS/undroidwish /usr/local/bin/.
+
+#export SDL_VIDEODRIVER=jsmpeg
+#export SDL_VIDEO_JSMPEG_OUTFILE=~/Desktop/decent.mpg
+undroidwish de1plus.tcl  -sdlheight 800 -sdlwidth 1280 -sdlrootheight 800 -sdlrootwidth 1280 -name Decent -sdlresizable
+
