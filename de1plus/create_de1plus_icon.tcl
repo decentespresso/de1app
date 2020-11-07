5de3d1a63838dcc7677a432425714400d44916bd
John Buckman <john@decentespresso.com>
2020-11-07 15:32:51 +0800

    fixed Android 10 issues creating the app shortcut

--- de1plus/create_de1plus_icon.tcl
+++ de1plus/create_de1plus_icon.tcl
@@ -3,8 +3,12 @@
 cd "[file dirname [info script]]/"
 source "pkgIndex.tcl"
 
-#set de1(has_flowmeter) 0
-#set ::de1(de1_address) "C1:80:A7:32:CD:A3"
+catch {
+	# john 4-11-20 Android 10 is failing on this script, if we don't include these two dependencies
+	package require snit
+	package require de1_updater
+}
+
 package require de1_main
 package require de1_gui
 
