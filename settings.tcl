9d4bb4d72c0a884628ed5989fd0b67d47cf53720
Mimoja <git@mimoja.de>
2021-01-26 16:18:22 +0100

    new purple theme

--- settings.tcl
+++ settings.tcl
@@ -2,47 +2,64 @@ array set ::default_theme {
     background "#FFFFFF"
     background_highlight "#EEEEEE"
 
-    primary "#3D5682"  
-    primary_light "#417491"  
-    primary_dark "#414A91"  
+    primary "#3D5682"
+    primary_light "#417491"
+    primary_dark "#414A91"
 
-    secondary "#F27405"  
-    secondary_light "#F28705"  
+    secondary "#F27405"
+    secondary_light "#F28705"
 
-
-    button "#3D5682"  
+    button "#3D5682"
     button_secondary "#F27405"
     button_tertiary "#182130"
 
     button_text_light "#eee"
-    button_text_dark "#111"
+    button_text_dark "#CCCCCC"
 }
 
 array set ::dark_theme {
     background "#121212"
     background_highlight "#121212"
 
-    primary "#BB86FC"  
-    primary_light "#BB86FC"  
-    primary_dark "#BB86FC"  
+    primary "#BB86FC"
+    primary_light "#BB86FC"
+    primary_dark "#BB86FC"
 
-    secondary "#03DAC6"  
-    secondary_light "#03DAC6"  
+    secondary "#03DAC6"
+    secondary_light "#03DAC6"
 
-
-    button "#1E1E1E"  
+    button "#1E1E1E"
     button_secondary "#1E1E1E"
     button_tertiary "#1E1E1E"
 
     button_text_light "#FFFFFF"
-    button_text_dark "#FFFFFF"
+    button_text_dark "#969eb1"
+}
+
+array set ::purple_theme {
+    background "#3c3c48"
+    background_highlight "#343444"
+
+    primary "#3c3a5c"
+    primary_light "#4f4d7a"
+    primary_dark "#32304d"
+
+    secondary "#e2a3b6"
+    secondary_light "#bf8a9a"
+
+    button "#b73f49"
+    button_secondary "#3d3b5e"
+    button_tertiary "#7a354b"
+
+    button_text_light "#eee"
+    button_text_dark "#CCCCCC"
 }
 
 
 # fonts
 set ::font_tiny [get_font "Mazzard Regular" 16]
 set ::font_small [get_font "Mazzard Regular" 18]
-set ::font_big [get_font "Mazzard Regular" 24]
+set ::font_big [get_font "Mazzard Regular" 22]
 
 array set ::iconik_settings {
     profile1  {default}
