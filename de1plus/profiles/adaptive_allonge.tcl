7c7638b86594e7128b97657ca4192092c840825a
John Buckman <john@decentespresso.com>
2021-01-29 16:43:47 +0800

    new adaptive profiles from Jonathan Gagné

---
+++ de1plus/profiles/adaptive_allonge.tcl
@@ -0,0 +1,36 @@
+advanced_shot {{exit_if 1 flow 4.00 volume 100 transition fast exit_flow_under 0 temperature 94.0 name preinfusion pressure 1 sensor coffee pump flow exit_type pressure_over exit_flow_over 6 exit_pressure_over 4.0 seconds 15.00 exit_pressure_under 0} {name {rise and hold} temperature 94.0 sensor coffee pump pressure transition fast pressure 8.6 seconds 4 volume 100 exit_if 0 exit_pressure_over 11 exit_pressure_under 0 exit_flow_over 6 exit_flow_under 0} {exit_if 1 flow 6.00 volume 100 transition fast exit_flow_under 5.90 temperature 94.0 name {Scan 6.0 mL.s} pressure 6.0 sensor coffee pump flow exit_type flow_under exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 127} {exit_if 1 flow 5.50 volume 100 transition fast exit_flow_under 5.40 temperature 94.0 name {Scan 5.5 mL.s} pressure 6.0 sensor coffee pump flow exit_type flow_under exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 127} {exit_if 1 flow 5.20 volume 100 transition fast exit_flow_under 5.10 temperature 94.0 name {Scan 5.2 mL.s} pressure 6.0 sensor coffee pump flow exit_type flow_under exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 127} {exit_if 1 flow 5.00 volume 100 transition fast exit_flow_under 4.90 temperature 94.0 name {Scan 5.0 mL.s} pressure 6.0 sensor coffee pump flow exit_type flow_under exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 127} {exit_if 1 flow 4.90 volume 100 transition fast exit_flow_under 4.80 temperature 94.0 name {Scan 4.9 mL.s} pressure 6.0 sensor coffee pump flow exit_type flow_under exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 127} {exit_if 1 flow 4.80 volume 100 transition fast exit_flow_under 4.70 temperature 94.0 name {Scan 4.8 mL.s} pressure 6.0 sensor coffee pump flow exit_type flow_under exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 127} {exit_if 1 flow 4.70 volume 100 transition fast exit_flow_under 4.60 temperature 94.0 name {Scan 4.7 mL.s} pressure 6.0 sensor coffee pump flow exit_type flow_under exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 127} {exit_if 1 flow 4.60 volume 100 transition fast exit_flow_under 4.50 temperature 94.0 name {Scan 4.6 mL.s} pressure 6.0 sensor coffee pump flow exit_type flow_under exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 127} {exit_if 1 flow 4.50 volume 100 transition fast exit_flow_under 4.40 temperature 94.0 name {Scan 4.5 mL.s} pressure 6.0 sensor coffee pump flow exit_type flow_under exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 127} {exit_if 1 flow 4.40 volume 100 transition fast exit_flow_under 4.30 temperature 94.0 name {Scan 4.4 mL.s} pressure 6.0 sensor coffee pump flow exit_type flow_under exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 127} {exit_if 1 flow 4.30 volume 100 transition fast exit_flow_under 4.20 temperature 94.0 name {Scan 4.3 mL.s} pressure 6.0 sensor coffee pump flow exit_type flow_under exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 127} {exit_if 1 flow 4.20 volume 100 transition fast exit_flow_under 4.10 temperature 94.0 name {Scan 4.2 mL.s} pressure 6.0 sensor coffee pump flow exit_type flow_under exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 127} {exit_if 1 flow 4.10 volume 100 transition fast exit_flow_under 4.00 temperature 94.0 name {Scan 4.1 mL.s} pressure 6.0 sensor coffee pump flow exit_type flow_under exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 127} {exit_if 1 flow 4.00 volume 100 transition fast exit_flow_under 3.90 temperature 94.0 name {Scan 4.0 mL.s} pressure 6.0 sensor coffee pump flow exit_type flow_under exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 127} {exit_if 1 flow 3.80 volume 100 transition fast exit_flow_under 3.70 temperature 94.0 name {Scan 3.8 mL.s} pressure 6.0 sensor coffee pump flow exit_type flow_under exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 127} {exit_if 1 flow 3.40 volume 100 transition fast exit_flow_under 3.30 temperature 94.0 name {Scan 3.4 mL.s} pressure 6.0 sensor coffee pump flow exit_type flow_under exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 127} {exit_if 1 flow 3.00 volume 100 transition fast exit_flow_under 2.90 temperature 94.0 name {Scan 3.0 mL.s} pressure 6.0 sensor coffee pump flow exit_type flow_under exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 127} {exit_if 0 flow 1.00 volume 100 transition fast exit_flow_under 1.80 temperature 94.0 name {last resort} pressure 6.0 sensor coffee pump flow exit_type flow_under exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 127}}
+author Gagne
+espresso_decline_time 35
+espresso_hold_time 4
+espresso_pressure 8.6
+espresso_temperature 88.0
+espresso_temperature_0 90.0
+espresso_temperature_1 88.0
+espresso_temperature_2 88.0
+espresso_temperature_3 88.0
+espresso_temperature_steps_enabled 1
+final_desired_shot_volume 36
+final_desired_shot_volume_advanced 0
+final_desired_shot_volume_advanced_count_start 2
+final_desired_shot_weight 36
+final_desired_shot_weight_advanced 0
+flow_profile_decline 1.2
+flow_profile_decline_time 17
+flow_profile_hold 2
+flow_profile_hold_time 8
+flow_profile_minimum_pressure 4
+flow_profile_preinfusion 4
+flow_profile_preinfusion_time 5
+preinfusion_flow_rate 4
+preinfusion_guarantee 0
+preinfusion_stop_pressure 4.0
+preinfusion_time 20
+pressure_end 6.0
+profile_hide 1
+profile_language en
+profile_notes {This profile is a variation on the Rao Allongé. After preinfusion, pressure raises to 8.6 bar and then detects the current flow to keep it stable ever after. I believe this might make better espresso when it is not perfectly dialed in. See it like this - you are dialing in the constant flow rate, not the pressure curve or the shape of the flow curve. Faster flow rates might taste better with longer shots. Taken to the extreme, this profile can do something similar to the Rao Allonge if you just grind coarse enough.}
+profile_title {Gagné/Adaptive Allongé 94C v1.0}
+settings_profile_type settings_2c
+tank_desired_water_temperature 0
+water_temperature 80
+
---
+++ de1plus/profiles/adaptive_espresso.tcl
@@ -0,0 +1,36 @@
+advanced_shot {{exit_if 1 flow 3.50 volume 100 transition fast exit_flow_under 0 temperature 92.0 name preinfusion pressure 1 sensor coffee pump flow exit_type pressure_over exit_flow_over 6 exit_pressure_over 4.0 seconds 15.00 exit_pressure_under 0} {name {rise and hold} temperature 92.0 sensor coffee pump pressure transition fast pressure 8.6 seconds 4 volume 100 exit_if 0 exit_pressure_over 11 exit_pressure_under 0 exit_flow_over 6 exit_flow_under 0} {exit_if 1 flow 3.50 volume 100 transition fast exit_flow_under 3.40 temperature 92.0 name {Scan 3.5 mL.s} pressure 6.0 sensor coffee pump flow exit_type flow_under exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 127} {exit_if 1 flow 3.30 volume 100 transition fast exit_flow_under 3.20 temperature 92.0 name {Scan 3.3 mL.s} pressure 6.0 sensor coffee pump flow exit_type flow_under exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 127} {exit_if 1 flow 3.10 volume 100 transition fast exit_flow_under 3.00 temperature 92.0 name {Scan 3.1 mL.s} pressure 6.0 sensor coffee pump flow exit_type flow_under exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 127} {exit_if 1 flow 2.90 volume 100 transition fast exit_flow_under 2.80 temperature 92.0 name {Scan 2.9 mL.s} pressure 6.0 sensor coffee pump flow exit_type flow_under exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 127} {exit_if 1 flow 2.70 volume 100 transition fast exit_flow_under 2.60 temperature 92.0 name {Scan 2.7 mL.s} pressure 6.0 sensor coffee pump flow exit_type flow_under exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 127} {exit_if 1 flow 2.60 volume 100 transition fast exit_flow_under 2.50 temperature 92.0 name {Scan 2.6 mL.s} pressure 6.0 sensor coffee pump flow exit_type flow_under exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 127} {exit_if 1 flow 2.50 volume 100 transition fast exit_flow_under 2.40 temperature 92.0 name {Scan 2.5 mL.s} pressure 6.0 sensor coffee pump flow exit_type flow_under exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 127} {exit_if 1 flow 2.40 volume 100 transition fast exit_flow_under 2.30 temperature 92.0 name {Scan 2.4 mL.s} pressure 6.0 sensor coffee pump flow exit_type flow_under exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 127} {exit_if 1 flow 2.30 volume 100 transition fast exit_flow_under 2.20 temperature 92.0 name {Scan 2.3 mL.s} pressure 6.0 sensor coffee pump flow exit_type flow_under exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 127} {exit_if 1 flow 2.20 volume 100 transition fast exit_flow_under 2.10 temperature 92.0 name {Scan 2.2 mL.s} pressure 6.0 sensor coffee pump flow exit_type flow_under exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 127} {exit_if 1 flow 2.10 volume 100 transition fast exit_flow_under 2.00 temperature 92.0 name {Scan 2.1 mL.s} pressure 6.0 sensor coffee pump flow exit_type flow_under exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 127} {exit_if 1 flow 2.00 volume 100 transition fast exit_flow_under 1.90 temperature 92.0 name {Scan 2.0 mL.s} pressure 6.0 sensor coffee pump flow exit_type flow_under exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 127} {exit_if 1 flow 1.80 volume 100 transition fast exit_flow_under 1.70 temperature 92.0 name {Scan 1.8 mL.s} pressure 6.0 sensor coffee pump flow exit_type flow_under exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 127} {exit_if 1 flow 1.60 volume 100 transition fast exit_flow_under 1.50 temperature 92.0 name {Scan 1.6 mL.s} pressure 6.0 sensor coffee pump flow exit_type flow_under exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 127} {exit_if 1 flow 1.40 volume 100 transition fast exit_flow_under 1.30 temperature 92.0 name {Scan 1.4 mL.s} pressure 6.0 sensor coffee pump flow exit_type flow_under exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 127} {exit_if 1 flow 1.20 volume 100 transition fast exit_flow_under 1.10 temperature 92.0 name {Scan 1.2 mL.s} pressure 6.0 sensor coffee pump flow exit_type flow_under exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 127} {exit_if 1 flow 1.00 volume 100 transition fast exit_flow_under 0.90 temperature 92.0 name {Scan 1.0 mL.s} pressure 6.0 sensor coffee pump flow exit_type flow_under exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 127} {exit_if 0 flow 0.50 volume 100 transition fast exit_flow_under 1.80 temperature 92.0 name {last resort} pressure 6.0 sensor coffee pump flow exit_type flow_under exit_flow_over 6 exit_pressure_over 11 exit_pressure_under 0 seconds 127}}
+author Gagne
+espresso_decline_time 35
+espresso_hold_time 4
+espresso_pressure 8.6
+espresso_temperature 88.0
+espresso_temperature_0 90.0
+espresso_temperature_1 88.0
+espresso_temperature_2 88.0
+espresso_temperature_3 88.0
+espresso_temperature_steps_enabled 1
+final_desired_shot_volume 36
+final_desired_shot_volume_advanced 0
+final_desired_shot_volume_advanced_count_start 2
+final_desired_shot_weight 36
+final_desired_shot_weight_advanced 0
+flow_profile_decline 1.2
+flow_profile_decline_time 17
+flow_profile_hold 2
+flow_profile_hold_time 8
+flow_profile_minimum_pressure 4
+flow_profile_preinfusion 4
+flow_profile_preinfusion_time 5
+preinfusion_flow_rate 4
+preinfusion_guarantee 0
+preinfusion_stop_pressure 4.0
+preinfusion_time 20
+pressure_end 6.0
+profile_hide 1
+profile_language en
+profile_notes {This profile does an espresso-focused preinfusion, raises to 8.6 bar and then detects the current flow to keep it stable ever after. I believe this might make better espresso when it is not perfectly dialed in. See it like this - you are dialing in the constant flow rate, not the pressure curve or the shape of the flow curve. Faster flow rates might taste better with longer shots. Taken to the extreme, this profile can do something similar to the Rao Allonge if you just grind coarse enough.}
+profile_title {Gagné/Adaptive Shot 92C v1.0}
+settings_profile_type settings_2c
+tank_desired_water_temperature 0
+water_temperature 80
+
