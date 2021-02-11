a0224ae9cf23a42d23d198f6b89e0049d621c419
John Buckman <john@decentespresso.com>
2021-02-11 15:25:22 +0800

    added "forward flush" cleaning profile

---
+++ de1plus/profiles/cleaning_forward_flush.tcl
@@ -0,0 +1,41 @@
+advanced_shot {{exit_if 1 flow 4.00 volume 100 transition fast exit_flow_under 0 temperature 85.00 name {Pressure rise 1} pressure 6.80 sensor coffee pump flow exit_type pressure_over exit_flow_over 6 exit_pressure_over 7.00 exit_pressure_under 0 seconds 15.00} {exit_if 0 flow 6.50 volume 100 transition fast exit_flow_under 0 temperature 85.00 name {Pause 1} pressure 0 sensor coffee pump pressure exit_type pressure_over exit_flow_over 6 exit_pressure_over 7.00 exit_pressure_under 0 seconds 15.00} {exit_if 1 flow 6.50 volume 100 transition fast exit_flow_under 0 temperature 85.00 name {Pressure rise 2} pressure 6.80 sensor coffee pump flow exit_type pressure_over exit_flow_over 6 exit_pressure_over 7.00 exit_pressure_under 0 seconds 15.00} {exit_if 0 flow 6.50 volume 100 transition fast exit_flow_under 0 temperature 85.00 name {Pause 2} pressure 0 sensor coffee pump pressure exit_type pressure_over exit_flow_over 6 exit_pressure_over 7.00 exit_pressure_under 0 seconds 15.00} {exit_if 1 flow 6.50 volume 100 transition fast exit_flow_under 0 temperature 85.00 name {Pressure rise 3} pressure 6.80 sensor coffee pump flow exit_type pressure_over exit_flow_over 6 exit_pressure_over 7.00 seconds 15.00 exit_pressure_under 0}}
+espresso_temperature_steps_enabled 0
+author Decent
+espresso_hold_time 10
+preinfusion_time 20
+espresso_pressure 8.4
+espresso_decline_time 30
+pressure_end 6.0
+espresso_temperature 85.00
+espresso_temperature_0 85.00
+espresso_temperature_1 85.00
+espresso_temperature_2 85.00
+espresso_temperature_3 85.00
+settings_profile_type settings_2c
+flow_profile_preinfusion 4.2
+flow_profile_preinfusion_time 6
+flow_profile_hold 4.0
+flow_profile_hold_time 2
+flow_profile_decline 1
+flow_profile_decline_time 23
+flow_profile_minimum_pressure 6
+preinfusion_flow_rate 3.5
+profile_notes {We recommend this pressure profile as the most likely to produce a good espresso in the most varied number of cases.  The decreasing pressure will help reduce acidity.}
+water_temperature 76
+final_desired_shot_volume 36
+final_desired_shot_weight 36
+final_desired_shot_weight_advanced 36
+tank_desired_water_temperature 0
+final_desired_shot_volume_advanced 36
+profile_title {Cleaning/Forward flush}
+profile_language en
+preinfusion_stop_pressure 4
+profile_hide 0
+final_desired_shot_volume_advanced_count_start 2
+bean_brand {}
+bean_type {}
+grinder_setting {}
+grinder_dose_weight 0
+grinder_model {}
+beverage_type espresso
+
