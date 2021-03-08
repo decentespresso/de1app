8ff2e368e3ef49fbb004412b2cf10b51e1b97602
Mimoja <git@mimoja.de>
2021-03-08 10:11:15 +0100

    Fix limiter changes in the advanced profiles
    Signed-off-by: Mimoja <git@mimoja.de>

--- de1plus/skins/default/de1_skin_settings.tcl
+++ de1plus/skins/default/de1_skin_settings.tcl
@@ -121,7 +121,7 @@ add_de1_text "settings_2a" 890 755 -text [translate "2: rise and hold"] -font He
 
 # Flow limit
 add_de1_text "settings_2a" 890 1100 -text [translate "3: (BETA!) limit flow"] -font Helv_10_bold -fill "#7f879a" -anchor "nw" -width 600 -justify "left" 
-	add_de1_widget "settings_2a" scale 892 1175 {} -from 0 -to 12 -background #e4d1c1 -borderwidth 1 -showvalue 0  -bigincrement 1 -resolution 0.1 -length [rescale_x_skin 546] -width [rescale_y_skin 75] -variable ::settings(maximum_flow) -font Helv_10_bold -sliderlength [rescale_x_skin 125] -relief flat -command "" -orient horizontal -foreground #FFFFFF -troughcolor $slider_trough_color -borderwidth 0  -highlightthickness 0 
+	add_de1_widget "settings_2a" scale 892 1175 {} -from 0 -to 8 -background #e4d1c1 -borderwidth 1 -showvalue 0  -bigincrement 1 -resolution 0.1 -length [rescale_x_skin 546] -width [rescale_y_skin 75] -variable ::settings(maximum_flow) -font Helv_10_bold -sliderlength [rescale_x_skin 125] -relief flat -command "" -orient horizontal -foreground #FFFFFF -troughcolor $slider_trough_color -borderwidth 0  -highlightthickness 0 
 	add_de1_widget "settings_2a" scale 892 1250 {} -from 0 -to 6 -background #e4d1c1 -showvalue 0 -borderwidth 1 -bigincrement 1 -resolution 0.1 -length [rescale_x_skin 546] -width [rescale_y_skin 75] -variable ::settings(maximum_flow_range) -font Helv_15_bold -sliderlength [rescale_x_skin 125] -relief flat -command "range_check_shot_variables; profile_has_changed_set; update_de1_explanation_chart_soon" -foreground #FFFFFF -troughcolor $slider_trough_color -borderwidth 0  -highlightthickness 0 -orient horizontal 
 
 add_de1_variable "settings_2a" 892 1325 -text "" -font Helv_8 -fill "#4e85f4" -anchor "nw" -width 600 -justify "left" -textvariable {[settings_flow_label]}
@@ -369,9 +369,9 @@ proc settings2c_pressure_button {direction} {
 		}
 	} else { 
 		if {$direction eq "up"} {
-			set ::current_adv_step(max_flow_or_pressure) [expr {$::current_adv_step(max_flow_or_pressure) + 0.5}];
+			set ::current_adv_step(max_flow_or_pressure) [round_to_one_digits [expr {$::current_adv_step(max_flow_or_pressure) + 0.1}]];
 		} else {
-			set ::current_adv_step(max_flow_or_pressure) [expr {$::current_adv_step(max_flow_or_pressure) - 0.5}];
+			set ::current_adv_step(max_flow_or_pressure) [round_to_one_digits [expr {$::current_adv_step(max_flow_or_pressure) - 0.1}]];
 		}
 	}
 	update_onscreen_variables
@@ -390,9 +390,9 @@ proc settings2c_flow_button {direction} {
 		}
 	} else { 
 		if {$direction eq "up"} {
-			set ::current_adv_step(max_flow_or_pressure) [expr {$::current_adv_step(max_flow_or_pressure) + 0.1}];
+			set ::current_adv_step(max_flow_or_pressure) [round_to_one_digits [expr {$::current_adv_step(max_flow_or_pressure) + 0.1}]];
 		} else {
-			set ::current_adv_step(max_flow_or_pressure) [expr {$::current_adv_step(max_flow_or_pressure) - 0.1}];
+			set ::current_adv_step(max_flow_or_pressure) [round_to_one_digits [expr {$::current_adv_step(max_flow_or_pressure) - 0.1}]];
 		}
 	}
 	update_onscreen_variables
