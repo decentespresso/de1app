63910ca9725321a175d64468e126cf3e5fcdd7c1
John Buckman <john@decentespresso.com>
2021-03-16 10:36:47 +0800

    New 7g basket recipe

---
+++ de1plus/profiles/7g basket.tcl
@@ -0,0 +1,42 @@
+advanced_shot {{name preinfusion temperature 92.0 sensor coffee pump flow transition fast pressure 1 flow 4.5 seconds 0 volume 100 exit_if 1 exit_type pressure_over exit_pressure_over 4.0 exit_pressure_under 0 exit_flow_over 6 exit_flow_under 0} {name preinfusion temperature 92.0 sensor coffee pump flow transition fast pressure 1 flow 4.5 seconds 20 volume 100 exit_if 1 exit_type pressure_over exit_pressure_over 4.0 exit_pressure_under 0 exit_flow_over 6 exit_flow_under 0} {name {rise and hold} temperature 92.0 sensor coffee pump pressure transition fast pressure 9.0 seconds 8 volume 100 exit_if 0 exit_pressure_over 11 exit_pressure_under 0 exit_flow_over 6 exit_flow_under 0 max_flow_or_pressure 8.0 max_flow_or_pressure_range 0.6} {name decline temperature 92.0 sensor coffee pump pressure transition smooth pressure 0.0 seconds 27 volume 100 exit_if 0 exit_pressure_over 11 exit_pressure_under 0 exit_flow_over 6 exit_flow_under 0 max_flow_or_pressure 8.0 max_flow_or_pressure_range 0.6}}
+author Decent
+beverage_type espresso
+espresso_decline_time 10
+espresso_hold_time 6
+espresso_pressure 9.0
+espresso_temperature 90.0
+espresso_temperature_0 90.0
+espresso_temperature_1 90.0
+espresso_temperature_2 90.0
+espresso_temperature_3 90.0
+espresso_temperature_steps_enabled 0
+final_desired_shot_volume 36
+final_desired_shot_volume_advanced 36
+final_desired_shot_volume_advanced_count_start 2
+final_desired_shot_weight 28
+final_desired_shot_weight_advanced 36
+flow_profile_decline 1
+flow_profile_decline_time 23
+flow_profile_hold 2.3
+flow_profile_hold_time 2
+flow_profile_minimum_pressure 6
+flow_profile_preinfusion 4.2
+flow_profile_preinfusion_time 6
+maximum_flow 6.0
+maximum_flow_range 0.6
+maximum_flow_range_advanced 0.6
+maximum_pressure 12.0
+maximum_pressure_range 0.6
+maximum_pressure_range_advanced 0.6
+preinfusion_flow_rate 2.4
+preinfusion_stop_pressure 4.5
+preinfusion_time 8
+pressure_end 4.5
+profile_hide 1
+profile_language en
+profile_notes {This profile is optimized for the 7g Decent Basket.  Because it takes less than half as much coffee grounds as the 15g basket, which the Default profile is optimized for.  You should weigh out 7g of beans and dose them into the center mini-basket at the bottom.  You'll need a small tamper, not the usual 58mm one.  The flow rate is slowed down to compensate for the smaller dose and to prevent disturbing the puck.  A lower pressure is used to compensate for the lower puck integrity.  Final shot weight should be between 18 to 28 grams.}
+profile_title {7g basket}
+settings_profile_type settings_2a
+tank_desired_water_temperature 0
+water_temperature 80
+
