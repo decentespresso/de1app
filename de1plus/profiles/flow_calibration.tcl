99934fa884a3b19c6fdc4853b19d9e8edf20076a
John Buckman <john@decentespresso.com>
2021-03-24 14:33:32 +0800

    hide flow calibration profile by default

--- de1plus/profiles/flow_calibration.tcl
+++ de1plus/profiles/flow_calibration.tcl
@@ -1,42 +1,42 @@
 advanced_shot {{exit_if 0 flow 1.5 volume 0 max_flow_or_pressure_range 0.6 transition fast exit_flow_under 0 temperature 90.00 name 1 pressure 1 sensor coffee pump pressure exit_type pressure_over exit_flow_over 0 exit_pressure_over 10.0 max_flow_or_pressure 0 exit_pressure_under 0 seconds 15.00} {exit_if 0 flow 1.5 volume 0 max_flow_or_pressure_range 0.6 transition fast exit_flow_under 0 temperature 90.00 name 1 pressure 2.000000000000001 sensor coffee pump pressure exit_type pressure_over exit_flow_over 0 exit_pressure_over 10.0 max_flow_or_pressure 0 seconds 10.00 exit_pressure_under 0} {exit_if 0 flow 1.5 volume 0 max_flow_or_pressure_range 0.6 transition fast exit_flow_under 0 temperature 90.00 name 1 pressure 3.0000000000000018 sensor coffee pump pressure exit_type pressure_over exit_flow_over 0 exit_pressure_over 10.0 max_flow_or_pressure 0 exit_pressure_under 0 seconds 10.00} {exit_if 0 flow 1.5 volume 0 max_flow_or_pressure_range 0.6 transition fast exit_flow_under 0 temperature 90.00 name 1 pressure 4.000000000000003 sensor coffee pump pressure exit_type pressure_over exit_flow_over 0 exit_pressure_over 10.0 max_flow_or_pressure 0 seconds 10.00 exit_pressure_under 0} {exit_if 0 flow 1.5 volume 0 max_flow_or_pressure_range 0.6 transition fast exit_flow_under 0 temperature 90.00 name 1 pressure 4.999999999999999 sensor coffee pump pressure exit_type pressure_over exit_flow_over 0 exit_pressure_over 10.0 max_flow_or_pressure 0 exit_pressure_under 0 seconds 10.00} {exit_if 0 flow 1.5 volume 0 max_flow_or_pressure_range 0.6 transition fast exit_flow_under 0 temperature 90.00 name 1 pressure 5.999999999999996 sensor coffee pump pressure exit_type pressure_over exit_flow_over 0 exit_pressure_over 10.0 max_flow_or_pressure 0 seconds 10.00 exit_pressure_under 0} {exit_if 0 flow 1.5 volume 0 max_flow_or_pressure_range 0.6 transition fast exit_flow_under 0 temperature 90.00 name 1 pressure 6.999999999999992 sensor coffee pump pressure exit_type pressure_over exit_flow_over 0 exit_pressure_over 10.0 max_flow_or_pressure 0 exit_pressure_under 0 seconds 10.00}}
-espresso_temperature_steps_enabled 1
 author Decent
-espresso_hold_time 30
-preinfusion_time 60
-espresso_pressure 10.0
+beverage_type calibrate
 espresso_decline_time 30
-pressure_end 10.0
+espresso_hold_time 30
+espresso_pressure 10.0
 espresso_temperature 90.00
 espresso_temperature_0 0.0
 espresso_temperature_1 0.0
 espresso_temperature_2 0.0
 espresso_temperature_3 0.0
-settings_profile_type settings_2c
-flow_profile_preinfusion 4
-flow_profile_preinfusion_time 5
-flow_profile_hold 2
-flow_profile_hold_time 8
-flow_profile_decline 1.2
-flow_profile_decline_time 17
-flow_profile_minimum_pressure 4
-preinfusion_flow_rate 1.5
-profile_notes {Put a 0.3mm puck simulator basket into a portafilter. Connect a bluetooth scale.  Run this profile. Change the Flow value on the Calibration page, and rerun this profile.  Repeat until the blue and brown lines follow each other.}
-water_temperature 70.00
+espresso_temperature_steps_enabled 1
 final_desired_shot_volume 0
+final_desired_shot_volume_advanced 0
+final_desired_shot_volume_advanced_count_start 2
 final_desired_shot_weight 32
 final_desired_shot_weight_advanced 0
-tank_desired_water_temperature 0
-final_desired_shot_volume_advanced 0
-profile_title {Test/Flow calibration}
-profile_language en
-preinfusion_stop_pressure 10.0
-profile_hide 0
-final_desired_shot_volume_advanced_count_start 2
-beverage_type calibrate
+flow_profile_decline 1.2
+flow_profile_decline_time 17
+flow_profile_hold 2
+flow_profile_hold_time 8
+flow_profile_minimum_pressure 4
+flow_profile_preinfusion 4
+flow_profile_preinfusion_time 5
+maximum_flow 0
+maximum_flow_range 0.6
+maximum_flow_range_advanced 0.6
 maximum_pressure 0
 maximum_pressure_range 0.6
-maximum_flow_range 0.6
 maximum_pressure_range_advanced 0.6
-maximum_flow_range_advanced 0.6
-maximum_flow 0
+preinfusion_flow_rate 1.5
+preinfusion_stop_pressure 10.0
+preinfusion_time 60
+pressure_end 10.0
+profile_hide 1
+profile_language en
+profile_notes {Put a 0.3mm puck simulator basket into a portafilter. Connect a bluetooth scale.  Run this profile. Change the Flow value on the Calibration page, and rerun this profile.  Repeat until the blue and brown lines follow each other.}
+profile_title {Test/Flow calibration}
+settings_profile_type settings_2c
+tank_desired_water_temperature 0
+water_temperature 70.00
 
