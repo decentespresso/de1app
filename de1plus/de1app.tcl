58f5555a9a348850ad97af62734c1ee997219aa4
Jeff Kletsky <git-commits@allycomm.com>
2021-03-29 19:39:47 -0700

    build-info: Read build-info, log, and make available in ::app
    The build-info file that can be generated in the build process
    contains detailed information about the build that can uniquely
    identofy the contents of the build both in machine-readable and
    human-readable forms.
    
    Read in the build-info file if it is readable, output to the log,
    and read into the ::app::build_info dict for other uses.
    
    Provide ::app::version_string and ::app_build_timestamp from
    build-info, using version.tcl and timestamp.txt as fallbacks.
    
    Signed-Off-By: Jeff Kletsky <git-commits@allycomm.com>

--- de1plus/de1app.tcl
+++ de1plus/de1app.tcl
@@ -18,6 +18,93 @@ try {
 	exit
 }
 
+#
+# Inline for now, then move out
+#
+
+namespace eval ::app {
+
+	variable build_info_filename "build-info"
+	variable build_info
+	set build_info [dict create]
+
+	variable build_string ""
+	variable build_timestamp 0
+
+	proc load_build_info {} {
+
+		variable build_info_filename
+		variable build_info
+
+		if { [file readable $build_info_filename] } {
+
+			set _fh [open $build_info_filename "r"]
+
+			foreach _line [split [read $_fh] "\n"] {
+
+				msg -NOTICE "build-info: $_line"
+
+				if { [string length $_line] == 0 } { continue }
+				if { [regexp {^[:space:]*#} $_line] } { continue }
+
+				set _kv [split $_line "\t"]
+				set _k [lindex $_kv 0]
+				if { [llength $_kv] == 1 } {
+					set _v ""
+				} else {
+					set _v [join [lrange $_kv 1 end] "\t"]
+				}
+
+				dict append build_info $_k $_v
+			}
+		} else {
+
+			msg -WARNING "build-info: No such file:" \
+				$build_info_filename
+		}
+	}
+
+	proc ensure_build_strings {} {
+
+		if { [dict exists $::app::build_info version_string] } {
+			set ::app::version_string [dict get $::app::build_info version_string]
+		} else {
+			set ::app::version_string [package version de1app]
+		}
+
+		if { [dict exists $::app::build_info build_timestamp] } {
+
+			set ::app::build_timestamp [dict get $::app::build_info build_timestamp]
+
+		} elseif { [file readable "[homedir]/timestamp.txt"] } {
+
+			set _fh [open "[homedir]/timestamp.txt" "r"]
+
+			set ::app::build_timestamp [string trim [read $_fh]]
+
+		} else {
+
+			set ::app::build_timestamp 0
+		}
+
+		# Use modified ISO 8601 (no T, add space before zone)
+
+		if { $::app::build_timestamp } {
+			set ::app::build_time_string [clock format $::app::build_timestamp \
+							      -format "%Y-%m-%d %H:%M:%S %z"]
+		} else {
+			set ::app::build_time_string [translate "Unknown"]
+		}
+	}
+
+}
+
+::app::load_build_info
+::app::ensure_build_strings
+
+msg -INFO "version_string: $::app::version_string"
+msg -INFO "build time: $::app::build_time_string"
+
 try {
 	de1_ui_startup
 } on error {result ropts} {
