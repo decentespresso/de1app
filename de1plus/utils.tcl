7899b4fd3b62f212b81c5ed3fa160d09351cc66c
Jeff Kletsky <git-commits@allycomm.com>
2021-04-07 16:59:15 -0700

    logging: Convert puts-logging to msg; bluetooth, de1_comms, gui, utils
    First pass at assigning reasonable severituy codes for msg in
    
      bluetooth.tcl
      de1_comms.tcl
      gui.tcl
      utils.tcl
    
    Commented-out lines removed
    
    No work done to ensure that no binary data is being logged.
    
        proc append_file {filename data} had a puts for everything
        appended.  As this can be binary, it was not converted and
        commented out.
    
        proc render_arabic {args} potentially dumps the input string on
        error which may not be able to be rendered in the logs.
        Retained at this time.
    
    These severities are likely to change in future commits,
    but need to get at least a first-cut at usability.
    
    DEVELOPER NOTES
    ===============
    
    Errors in calling add_de1_widget will now be logged rather than
    only appearing on the console.
    
    The log message has not been updated to include the options dict
    that could be returned by the catch {} statement around that call.
    
    Signed-Off-By: Jeff Kletsky <git-commits@allycomm.com>

--- de1plus/utils.tcl
+++ de1plus/utils.tcl
@@ -13,7 +13,6 @@ set SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN 0x00000400
 set ::android_full_screen_flags [expr {$SYSTEM_UI_FLAG_LAYOUT_STABLE | $SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | $SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | $SYSTEM_UI_FLAG_HIDE_NAVIGATION | $SYSTEM_UI_FLAG_FULLSCREEN | $SYSTEM_UI_FLAG_IMMERSIVE}]
 
 proc setup_environment {} {
-    #puts "setup_environment"
     global screen_size_width
     global screen_size_height
     global fontm
@@ -127,7 +126,6 @@ proc setup_environment {} {
         global helvetica_bold_font
         global helvetica_font
         set global_font_size 18
-        #puts "setting up fonts for language [language]"
         if {[language] == "th"} {
             set helvetica_font [sdltk addfont "fonts/sarabun.ttf"]
             set helvetica_bold_font [sdltk addfont "fonts/sarabunbold.ttf"]
@@ -359,7 +357,6 @@ proc check_battery_low {brightness_to_use} {
     #return 100
     set percent [battery_percent]
 
-    #puts "check_battery_low: $brightness_to_use / borg brightness = [borg brightness] / powerinfo(percent) = [ifexists powerinfo(percent)]"
     if {$percent < $::settings(battery_very_low_trigger)} {
         if {$current_brightness > $::settings(battery_very_low_brightness)} {
             get_set_tablet_brightness $::settings(battery_very_low_brightness)
@@ -475,7 +472,6 @@ proc random_saver_file {} {
 
 
     if {[info exists ::saver_files_cache] != 1} {
-        #puts "building saver_files_cache"
         set ::saver_files_cache {}
  
         set savers {}
@@ -596,7 +592,6 @@ proc tcl_introspection {} {
 proc random_splash_file {} {
     if {[info exists ::splash_files_cache] != 1} {
 
-        #puts "building splash_files_cache"
         set ::splash_files_cache {}
  
         set savers {}
@@ -619,7 +614,7 @@ proc random_splash_file {} {
                 photoscale saver $rescale_images_y_ratio $rescale_images_x_ratio
 
                 set resized_filename "[splash_directory]/${::screen_size_width}x${::screen_size_height}/[file tail $fn]"
-                puts "saving resized image to: $resized_filename"
+                msg -DEBUG "saving resized image to: $resized_filename"
                 borg spinner off
                 saver write $resized_filename   -format {jpeg -quality 50}
             }
@@ -634,7 +629,7 @@ proc random_splash_file {} {
 
 proc random_splash_file_obs {} {
     if {[info exists ::splash_files_cache] != 1} {
-        puts "building splash_files_cache"
+	msg -DEBUG "building splash_files_cache"
         set ::splash_files_cache {}
         if {[file exists "[splash_directory]/${::screen_size_width}x${::screen_size_height}/"] == 1} {
             set files [glob -nocomplain "[splash_directory]/${::screen_size_width}x${::screen_size_height}/*.jpg"]
@@ -649,7 +644,7 @@ proc random_splash_file_obs {} {
             }
         }
         borg spinner off
-        puts "savers: $::splash_files_cache"
+        msg -INFO "savers: $::splash_files_cache"
 
     }
 
@@ -750,8 +745,6 @@ proc translate {english} {
         return $english
     }
 
-    #puts "lang: '[language]'"
-
     global translation
 
     if {[info exists translation($english)] == 1} {
@@ -759,9 +752,7 @@ proc translate {english} {
         array set available $translation($english)
         if {[info exists available([language])] == 1} {
             # this word has been translated into the desired non-english language
-            #puts "$available([language])"
 
-            #puts "translate: '[encoding convertfrom $available([language])]'"
             if {[ifexists available([language])] != ""} {
                 # if the translated version of the English is NOT blank, return it
                 #log_to_debug_file [encoding names]
@@ -818,7 +809,6 @@ proc skin_directory {} {
         #set skindir "skinscreator"
     #}
 
-    #puts "skind: $skindir"
     #set dir "[file dirname [info script]]/$skindir/default/${screen_size_width}x${screen_size_height}"
     set dir "[homedir]/$skindir/$::settings(skin)"
     return $dir
@@ -854,7 +844,7 @@ proc android_specific_stubs {} {
         } elseif {[lindex $args 0] == "spinner"} {
             # do nothing
         } elseif {[lindex $args 0] == "toast"} {
-            puts "screen popup message: '$args'"
+            msg -NOTICE "screen popup message: '$args'"
         } elseif {[lindex $args 0] == "brightness"} {
             if {[lindex $args 1] == ""} {
                 return 70
@@ -863,7 +853,7 @@ proc android_specific_stubs {} {
             }
 
         } else {
-            puts "unknown 'borg $args'"
+            msg -ERROR "unknown 'borg $args'"
         }
     }
 }
@@ -913,13 +903,11 @@ proc skin_directory_graphics {} {
     #    set skindir "[homedir]/skinscreator"
     #}
 
-    #puts "skind: $skindir"
     set dir "$skindir/$::settings(skin)/${screen_size_width}x${screen_size_height}"
 
     if {[info exists ::rescale_images_x_ratio] == 1} {
         set dir "$skindir/$::settings(skin)/2560x1600"
     }
-    #puts "skindir '$skindir'"
     #set dir "[file dirname [info script]]/$skindir/default"
     return $dir
 }
@@ -939,13 +927,11 @@ proc defaultskin_directory_graphics {} {
     #    set skindir "[homedir]/skinscreator"
     #}
 
-    #puts "skind: $skindir"
     set dir "$skindir/default/${screen_size_width}x${screen_size_height}"
     
     if {[info exists ::rescale_images_x_ratio] == 1} {
         set dir "$skindir/default/2560x1600"
     }
-    #puts "skindir '$skindir'"
     #set dir "[file dirname [info script]]/$skindir/default"
     return $dir
 }
@@ -1099,7 +1085,10 @@ proc append_file {filename data} {
     set success 0
     set errcode [catch {
         set fn [open $filename a]
-        puts $fn $data 
+	#
+	# DISABLING this as it can print arbitrary binary to the logs
+	#
+	# puts $fn $data
         close $fn
         set success 1
     }]
@@ -1174,8 +1163,6 @@ proc save_settings {} {
 
 proc load_settings {} {
 
-
-    #puts "loading settings XXXXXXX"
 
     set osbuildinfo_string [borg osbuildinfo]
 
@@ -1264,7 +1251,6 @@ proc skin_yscale_factor {} {
 }
 
 proc rescale_x_skin {in} {
-    #puts "rescale_x_skin $in / [skin_xscale_factor]"
     return [expr {int($in / [skin_xscale_factor])}]
 }
 
@@ -1287,13 +1273,12 @@ proc skin_convert_all {} {
 }
 
 proc skin_convert {indir} {
-    #puts "skin_convert: $indir"
     set pwd [pwd]
     cd $indir
     set skinfiles [concat [glob -nocomplain "*.png"] [glob -nocomplain  "*.jpg"]]
 
     if {$skinfiles == ""} {
-        puts "No jpg files found in '$indir'"
+        msg -DEBUG "No jpg files found in '$indir'"
         return
     }
 
@@ -1313,14 +1298,12 @@ proc skin_convert {indir} {
 
     # convert all the skin PNG files
     foreach {dir xdivisor ydivisor} $dirs {
-        #puts -nonewline "Making $dir skin $xdivisor / $ydivisor"
         set started 0
 
 
         foreach skinfile $skinfiles {
             if {[file exists "../$dir/$skinfile"] == 1} {
                 if {[file mtime $skinfile] < [file mtime "../$dir/$skinfile"]} {
-                    #puts "skipping $skinfile [file exists "../$dir/$skinfile"]"
                     # skip files that have not been modified.
                     continue
                 }
@@ -1328,12 +1311,12 @@ proc skin_convert {indir} {
 
             if {$started == 0} {
                 set started 1
-                puts -nonewline "Making $dir skin $indir"
+                msg -DEBUG "skin_convert: Making $dir skin $indir"
             }
 
             file mkdir ../$dir
 
-            puts -nonewline "/$skinfile"
+            msg -DEBUG "skin_convert: /$skinfile"
             flush stdout
             if {[file extension $skinfile] == ".png"} {
                 catch {
@@ -1341,7 +1324,7 @@ proc skin_convert {indir} {
                     exec convert $skinfile -strip -resize $dir!   ../$dir/$skinfile 
                     
                     # additional optional PNG compression step 
-                    puts "zopflipng: $skinfile"
+		    msg -DEBUG "skin_convert:" "zopflipng: $skinfile"
                     #exec zopflipng -q --iterations=1 -y   ../$dir/$skinfile   ../$dir/$skinfile 
                     exec zopflipng -m --iterations=1 -y   ../$dir/$skinfile   ../$dir/$skinfile 
 
@@ -1352,7 +1335,7 @@ proc skin_convert {indir} {
 
                 catch {
                     exec convert $skinfile -resize $dir!  -quality 90 ../$dir/$skinfile 
-                    puts "\nconvert $skinfile -resize $dir!  -quality 90 ../$dir/$skinfile "
+                    msg -DEBUG "skin_convert:" "convert $skinfile -resize $dir!  -quality 90 ../$dir/$skinfile "
                 }
                 if {$skinfile == "icon.jpg"} {
                     # icon files are reduced to 25% of the screen resolution
@@ -1362,16 +1345,9 @@ proc skin_convert {indir} {
                 }
             }
         }
-
-
-        if {$started != 0} {
-            puts "";
-        }
-
     }
 
     cd $pwd
-
 }
 
 
@@ -1470,7 +1446,7 @@ proc load_font_obsolete {name fn pcsize {androidsize {}} } {
         set androidsize $pcsize
     }
 
-    puts "loadfont: [language]"
+    msg -DEBUG "loadfont: [language]"
  
     if {[language] == "zh-hant" || [language] == "zh-hans"} {
 
@@ -1478,7 +1454,6 @@ proc load_font_obsolete {name fn pcsize {androidsize {}} } {
             font create $name -family $::helvetica_font -size [expr {int(1.0 * $::fontm * $androidsize)}]
         } else {
             font create "$name" -family $::helvetica_font -size [expr {int(1.0 * $pcsize * $::fontm)}]
-            #puts "created font $name in $::helvetica_font"
         }
         return
     } elseif {[language] == "th"} {
@@ -1491,20 +1466,17 @@ proc load_font_obsolete {name fn pcsize {androidsize {}} } {
             font create $name -family $::thai_fontname -size [expr {int(1.0 * $::fontm * $androidsize)}]
         } else {
             font create "$name" -family "sarabun" -size [expr {int(1.0 * $pcsize * $::fontm)}]
-            #puts "created font $name in sarabun"
         }
         return
     } else {
-        #puts "$::android load_font $name '$fn' $size : fontm: $::fontm"
         if {$::android == 1 || $::undroid == 1} {
-            #puts "sdltk addfont '$fn'"
             set result ""
             catch {
                 set result [sdltk addfont $fn]
             }
             msg -DEBUG "addfont of '$fn' finished with fonts added: '$result'"
             if {$name != $result} {
-                puts "Warning, font name used does not equal Android font name added: '$name' != '$result'"
+                msg -WARNING "font name used does not equal Android font name added: '$name' != '$result'"
             }
             catch {
                 font create $name -family $name -size [expr {int(1.0 * $::fontm * $androidsize)}]
@@ -1521,9 +1493,6 @@ proc load_font_obsolete {name fn pcsize {androidsize {}} } {
 
 proc list_remove_element {list toremove} {
     set newlist [lsearch -all -inline -not -exact $list $toremove]
-    #puts "remove  :'$toremove'"
-    #puts "oldlist  :$list"
-    #puts "newlist: '$newlist'"
     return $newlist
 }
 
@@ -1562,7 +1531,6 @@ proc array_keyvalue_sorted_by_val_limited {arrname {sort_order -increasing} {lim
     foreach k [array names arr] {
         set k2 "$arr($k) $k"
         #set k2 "[format {"%0.12i"} $arr($k)] $k"
-        #puts "k2: $k2"
         set t($k2) $k
     }
     
@@ -1584,7 +1552,6 @@ proc shot_history_count_profile_use {} {
 
     set dirs [lsort -dictionary [glob -nocomplain -tails -directory "[homedir]/history/" *.shot]]
     set dd {}
-    #puts -nonewline "Exporting"
     foreach d $dirs {
         unset -nocomplain arr
         unset -nocomplain sett
@@ -1597,7 +1564,6 @@ proc shot_history_count_profile_use {} {
                 continue
             }
 
-        #puts [array get sett]
         #return
         set profile [ifexists sett(profile)]
         if {$profile != ""} {
@@ -1696,14 +1662,13 @@ proc export_csv {arrname fn} {
         } err
 
         if {$failed == 1} {
-            puts "$err: '$fn'"
+            msg -ERROR "export_csv:" "$err: '$fn'"
         }
 
     }
 
     #set newfile "[file rootname $rootname].csv"
-    #puts "$rootname, $newfile"
-    puts -nonewline "."
+    msg -DEBUG "export_csv about to write"
     #write_file "history/$newfile" $lines
     write_file $fn $lines
 
@@ -1726,7 +1691,7 @@ proc export_json {ftxt fn} {
     }
     set v [dict2json $d]
 
-    puts -nonewline "."
+    msg -DEBUG "export_json about to write"
     write_file "$fn" $v
 }
 # Export one shot from memory, to an EEX format file
@@ -1780,8 +1745,7 @@ meta,,,,,,,,,Export version,1.1.0,
     }
 
     #set newfile "[file rootname $rootname].csv"
-    #puts "$rootname, $newfile"
-    puts -nonewline "."
+    msg -DEBUG "export_csv_common_format about to write"
     #write_file "history/$newfile" $lines
     write_file $fn $lines
 
@@ -1802,7 +1766,7 @@ set length [string length $arabic_string]
             string range $arabic_string $start_of_ascii $end_of_ascii]] ==  1
             && $i<$length}  {
           
-                puts [
+                msg -DEBUG "list_of_all_ascii_parts_a_unicode_string:" [
                     string range $arabic_string $start_of_ascii $end_of_ascii]
                 incr i
                 incr end_of_ascii 
@@ -1920,9 +1884,8 @@ proc render_arabic args {
         #find the character before the last.
         
         set before_last_char [string index $word end-1]
-        
+
         #for debugging purposes just print the character before the last.
-        ## puts $before_last_char
         
         #and try to see if  the character before the last is a word in the list
         #$initials defined in the previous line.
@@ -1936,11 +1899,9 @@ proc render_arabic args {
             set last_character [string index $word end]
             
             #print it for debugging purposes
-            ##puts $last_character
             
             #just to make sure that we we are matching correctly print the unicode
             #index number of the character
-            ##puts [scan $last_character %c]
             if {[string is ascii $last_character]} {
                 set before_last_char [render_arabic $before_last_char]
             }
@@ -1970,7 +1931,7 @@ proc render_arabic args {
                 \u062c {
                     #jeem
                     set word [string replace $word end end \ufe9e]
-                    puts $word
+                    msg -DEBUG "render_arabic \\u062B:" $word
                 }
                 \u062d {
                     #7aa2
@@ -2089,14 +2050,16 @@ proc render_arabic args {
         #screen
 
         set failed 1
-        #puts stderr $arabic_string
         catch {
             set arabic_string [regsub -all "\\m$original_word\\M" $arabic_string $word]
             set failed 0
         } err
 
         if {$failed == 1} {
-            puts "$err: '$arabic_string'"
+	    #
+	    # Here have to hope that the string is "printable"
+	    #
+            msg -ERROR "render_arabic failed: $err: '$arabic_string'"
         }
 
         #add and replace the corrected/conversion-of word with malformed one. in
@@ -2111,7 +2074,6 @@ proc render_arabic args {
     #The following 2 line is left for you to see the final result. just remove
     #the comment sign (#)
     #tk_messageBox -message $words
-    #puts "before return: $arabic_string \n is_messageBox=$is_messageBox"
     
     #reverse the whole string
     set arabic_string [string reverse $arabic_string]
@@ -2147,11 +2109,11 @@ proc iso8601stringparse {in} {
     set time ""
     regexp {([0-9-]+)T([0-9:]+)\.?[0-9]+?Z} $in discard date time
     if {$date == ""} {
-        puts "No date found in: '$in'"
+        msg -ERROR "No date found in: '$in'"
         return 0
     }
     if {$time == ""} {
-        puts "No time found in: '$in'"
+        msg -ERROR "No time found in: '$in'"
         return 0
     }
     set timestring "$date $time UTC"
