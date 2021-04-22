d6825001525c1b7f00e8b72b32d3fffddbc527e0
Mimoja <git@mimoja.de>
2021-04-22 18:30:26 +0200

    Version 1.35 with Changelog post. YAY!
    Signed-off-by: Mimoja <git@mimoja.de>

--- de1plus/version.tcl
+++ de1plus/version.tcl
@@ -1,3 +1,3 @@
-package ifneeded de1app 1.34.34 {}
+package ifneeded de1app 1.35 {}
 
-set ::changelog_link "https://public.3.basecamp.com/p/XYiApo6ZGKjFaFHXsxoyx6Dz"
+set ::changelog_link "https://public.3.basecamp.com/p/RGr97jy2LtKyUsrT7NnctYSv"
