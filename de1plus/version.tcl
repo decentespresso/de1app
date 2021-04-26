c5f6241c7f3ae3f3cb575d57dbeff00f48f6c0c0
John Buckman <john@decentespresso.com>
2021-04-26 18:38:25 +0800

    updated v1.35.x release notes url

--- de1plus/version.tcl
+++ de1plus/version.tcl
@@ -1,3 +1,3 @@
 package ifneeded de1app 1.35.1 {}
 
-set ::changelog_link "https://public.3.basecamp.com/p/RGr97jy2LtKyUsrT7NnctYSv"
+set ::changelog_link "https://3.basecamp.com/3671212/buckets/7351439/messages/3703220672"
