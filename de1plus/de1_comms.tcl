bea292836c032b2e4dcf9158001baddf42174c56
Jeff Kletsky <git-commits@allycomm.com>
2021-04-29 16:11:20 -0700

    logging: Version bump to 1.2
    Reflect additions of the "sanatizing" functions
    
    Signed-Off-By: Jeff Kletsky <git-commits@allycomm.com>

--- de1plus/de1_comms.tcl
+++ de1plus/de1_comms.tcl
@@ -1,7 +1,7 @@
 package provide de1_comms 1.1
 
 package require de1_bluetooth
-package require de1_logging 1.0
+package require de1_logging 1.2
 
 ### Globals
 set ::failed_attempt_count_connecting_to_de1 0
