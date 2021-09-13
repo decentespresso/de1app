8d4b6050b1cf5e24926d8042ac9e924fca36a69c
Mimoja <git@mimoja.de>
2021-09-13 18:34:46 +0200

    Stable Version 1.37.0
    Signed-off-by: Mimoja <git@mimoja.de>

--- de1plus/version.tcl
+++ de1plus/version.tcl
@@ -1,4 +1,4 @@
 package ifneeded de1app 99.99.99 {}
 # The package version is set by the build script. This is just a dummy value
 
-set ::changelog_link "https://public.3.basecamp.com/p/M8uUwucqbNrL1fBB1JL9E6fF"
+set ::changelog_link "https://public.3.basecamp.com/p/GQVVtDpa7uKL7tUcp6KDe8Ma"
