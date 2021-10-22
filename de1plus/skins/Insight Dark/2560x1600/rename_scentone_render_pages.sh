1bd87e26a4073b9787089e32418fc487297aafe6
John Buckman <john@decentespresso.com>
2021-10-22 17:55:39 +0800

    new "Insight Dark" skin, a dark mode version of "Insight"

---
+++ de1plus/skins/Insight Dark/2560x1600/rename_scentone_render_pages.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+convert ~/Desktop/scentone_all0000.tif scentone_1.jpg
+convert ~/Desktop/scentone_all0001.tif scentone_tropical.jpg
+convert ~/Desktop/scentone_all0002.tif scentone_berry.jpg
+convert ~/Desktop/scentone_all0003.tif scentone_citrus.jpg
+
+convert ~/Desktop/scentone_all0004.tif scentone_stone.jpg
+convert ~/Desktop/scentone_all0005.tif scentone_cereal.jpg
+convert ~/Desktop/scentone_all0006.tif scentone_chocolate.jpg
+convert ~/Desktop/scentone_all0007.tif scentone_flower.jpg
+
+convert ~/Desktop/scentone_all0008.tif scentone_spice.jpg
+convert ~/Desktop/scentone_all0009.tif scentone_vegetable.jpg
+convert ~/Desktop/scentone_all0010.tif scentone_savory.jpg
+
+convert ~/Desktop/scentone_all0011.tif describe_espresso0.jpg
+convert ~/Desktop/scentone_all0012.tif describe_espresso.jpg
+convert ~/Desktop/scentone_all0013.tif describe_espresso2.jpg
+
+
+rm ~/Desktop/scentone_all0000.tif 
+rm ~/Desktop/scentone_all0001.tif 
+rm ~/Desktop/scentone_all0002.tif 
+rm ~/Desktop/scentone_all0003.tif 
+
+rm ~/Desktop/scentone_all0004.tif 
+rm ~/Desktop/scentone_all0005.tif 
+rm ~/Desktop/scentone_all0006.tif 
+rm ~/Desktop/scentone_all0007.tif 
+
+rm ~/Desktop/scentone_all0008.tif 
+rm ~/Desktop/scentone_all0009.tif 
+rm ~/Desktop/scentone_all0010.tif 
+
+rm ~/Desktop/scentone_all0011.tif 
+rm ~/Desktop/scentone_all0012.tif 
+rm ~/Desktop/scentone_all0013.tif 
+
