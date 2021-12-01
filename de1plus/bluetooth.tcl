29a8a0f8857c9d9880bebc82d3048a054482daa5
Mimoja <git@mimoja.de>
2021-12-01 18:57:52 +0100

    Try bluetooth GATT autoconnect flag when connecting to the scale
    Signed-off-by: Mimoja <git@mimoja.de>

--- de1plus/bluetooth.tcl
+++ de1plus/bluetooth.tcl
@@ -1140,11 +1140,11 @@ proc ble_connect_to_scale {} {
 		return
 	}
 
-	# 2021-11-25 Johanna: Removed to see if it is the cause for the lunar connection issues
-	#remove_matching_ble_queue_entries {^SCALE:}
+	remove_matching_ble_queue_entries {^SCALE:}
 
+	# 2021-11-25 Johanna: try autoconnect
 	if {[catch {
-		set ::currently_connecting_scale_handle [ble connect [string toupper $::settings(scale_bluetooth_address)] de1_ble_handler false]
+		set ::currently_connecting_scale_handle [ble connect [string toupper $::settings(scale_bluetooth_address)] de1_ble_handler true]
 		::bt::msg -NOTICE "Connecting to scale on $::settings(scale_bluetooth_address)"
 		set retcode 0
 	} err] != 0} {
