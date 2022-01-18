e4a96f597c18f6a435de4718b3feaacece428752
John Buckman <john@decentespresso.com>
2022-01-18 11:43:13 -0800

    clarified in the profile notes that Londonium and LRv2 are the same profile.

--- de1plus/profiles/Damian's LRv2.tcl
+++ de1plus/profiles/Damian's LRv2.tcl
@@ -5,8 +5,13 @@ espresso_decline_time 30
 espresso_hold_time 15
 espresso_pressure 6.0
 espresso_temperature 89.0
+espresso_temperature_0 89.0
+espresso_temperature_1 89.0
+espresso_temperature_2 89.0
+espresso_temperature_3 89.0
 final_desired_shot_volume 95
 final_desired_shot_volume_advanced 0
+final_desired_shot_volume_advanced_count_start 0
 final_desired_shot_weight 42
 final_desired_shot_weight_advanced 42
 flow_profile_decline 1.2
@@ -16,16 +21,20 @@ flow_profile_hold_time 8
 flow_profile_minimum_pressure 4
 flow_profile_preinfusion 4
 flow_profile_preinfusion_time 5
+maximum_flow 0
+maximum_flow_range_advanced 0.6
+maximum_flow_range_default 1.0
+maximum_pressure 0
+maximum_pressure_range_advanced 0.6
+maximum_pressure_range_default 0.9
 preinfusion_flow_rate 4
-preinfusion_guarantee 0
 preinfusion_stop_pressure 4.0
 preinfusion_time 20
 pressure_end 4.0
 profile_hide 1
 profile_language en
-profile_notes {This profile simulates a Londinium R machines extraction style. This is an advanced profile with some added steps to assist with less than ideal puck prep. Christee-Lee described it as like having a milkshake with extra syrup. Great body and flavour range. 2nd edition. By Damian Brakel https://www.diy.brakel.com.au/}
+profile_notes {This profile simulates a Londinium R machines extraction style. This is an advanced profile with some added steps to assist with less than ideal puck prep. Christee-Lee described it as like having a milkshake with extra syrup. Great body and flavour range. 2nd edition.  This is identical to the "Londonium" profile, but renamed to be easier to understand.  By Damian Brakel https://www.diy.brakel.com.au/}
 profile_title {Damian's LRv2}
 settings_profile_type settings_2c
 tank_desired_water_temperature 0
-water_temperature 80
 
