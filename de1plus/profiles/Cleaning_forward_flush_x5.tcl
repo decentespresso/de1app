cc945dd45d6ded79a5078805db78de0212b9d1a2
John Buckman <john@decentespresso.com>
2022-01-29 11:10:49 -0800

    New cleaning forward flush v5 profile revision

--- de1plus/profiles/Cleaning_forward_flush_x5.tcl
+++ de1plus/profiles/Cleaning_forward_flush_x5.tcl
@@ -1,42 +1,40 @@
-advanced_shot {{exit_if 0 flow 4.00 volume 500 max_flow_or_pressure_range 0.6 transition fast exit_flow_under 0 temperature 85.00 name {Pressure rise 1} pressure 9.999999999999993 sensor coffee pump pressure exit_type pressure_over exit_flow_over 6 exit_pressure_over 0 max_flow_or_pressure 0.0 seconds 20.00 exit_pressure_under 0} {exit_if 0 flow 6.50 volume 500 transition fast exit_flow_under 0 temperature 85.00 name {Pause 1} pressure 0 sensor coffee pump pressure exit_type pressure_over exit_flow_over 6 exit_pressure_over 7.00 exit_pressure_under 0 seconds 15.00} {exit_if 0 flow 6.50 volume 500 max_flow_or_pressure_range 0.6 transition fast exit_flow_under 0 temperature 85.00 name {Pressure rise 2} pressure 9.999999999999993 sensor coffee pump pressure exit_type pressure_over exit_flow_over 6 exit_pressure_over 7.00 max_flow_or_pressure 0.0 exit_pressure_under 0 seconds 15.00} {exit_if 0 flow 6.50 volume 500 max_flow_or_pressure_range 0.6 transition fast exit_flow_under 0 temperature 85.00 name {Pause 2} pressure -3.885780586188048e-16 sensor coffee pump pressure exit_type pressure_over exit_flow_over 6 exit_pressure_over 7.00 max_flow_or_pressure 0 seconds 15.00 exit_pressure_under 0} {exit_if 0 flow 6.50 volume 500 max_flow_or_pressure_range 0.6 transition fast exit_flow_under 0 temperature 85.00 name {Pressure rise 3} pressure 9.999999999999993 sensor coffee pump pressure exit_type pressure_over exit_flow_over 6 exit_pressure_over 7.00 max_flow_or_pressure 0 seconds 15.00 exit_pressure_under 0} {exit_if 0 flow 6.50 volume 500 max_flow_or_pressure_range 0.6 transition fast exit_flow_under 0 temperature 85.00 name {Pause 3} pressure -5.717648576819556e-15 sensor coffee pump pressure exit_type pressure_over exit_flow_over 6 exit_pressure_over 7.00 max_flow_or_pressure 0 exit_pressure_under 0 seconds 15.00} {exit_if 0 flow 6.50 volume 500 max_flow_or_pressure_range 0.6 transition fast exit_flow_under 0 temperature 85.00 name {Pressure rise 4} pressure 9.999999999999993 sensor coffee pump pressure exit_type pressure_over exit_flow_over 6 exit_pressure_over 7.00 max_flow_or_pressure 0 exit_pressure_under 0 seconds 15.00} {exit_if 0 flow 6.50 volume 500 max_flow_or_pressure_range 0.6 transition fast exit_flow_under 0 temperature 85.00 name {Pause 4} pressure -5.717648576819556e-15 sensor coffee pump pressure exit_type pressure_over exit_flow_over 6 exit_pressure_over 7.00 max_flow_or_pressure 0 seconds 15.00 exit_pressure_under 0} {exit_if 0 flow 6.50 volume 500 max_flow_or_pressure_range 0.6 transition fast exit_flow_under 0 temperature 85.00 name {Pressure rise 5} pressure 9.999999999999993 sensor coffee pump pressure exit_type pressure_over exit_flow_over 6 exit_pressure_over 7.00 max_flow_or_pressure 0 exit_pressure_under 0 seconds 15.00}}
-author Decent
-beverage_type cleaning
-espresso_decline_time 30
-espresso_hold_time 10
-espresso_pressure 8.4
-espresso_temperature 85.00
-espresso_temperature_0 85.00
-espresso_temperature_1 85.00
-espresso_temperature_2 85.00
-espresso_temperature_3 85.00
+advanced_shot {{exit_if 0 flow 4.00 volume 500 max_flow_or_pressure_range 0.6 transition fast exit_flow_under 0 temperature 85.00 name {Pressure rise 1} pressure 9.999999999999993 sensor coffee pump pressure exit_type pressure_over exit_flow_over 6 exit_pressure_over 0 max_flow_or_pressure 0.0 seconds 20.00 exit_pressure_under 0} {exit_if 0 flow 6.50 volume 500 max_flow_or_pressure_range 0.6 transition fast exit_flow_under 0 temperature 85.00 name {Pause 1} pressure 0 sensor coffee pump pressure exit_type pressure_over exit_flow_over 6 exit_pressure_over 7.00 max_flow_or_pressure 0.0 seconds 15.00 exit_pressure_under 0} {exit_if 0 flow 6.50 volume 500 max_flow_or_pressure_range 0.6 transition fast exit_flow_under 0 temperature 85.00 name {Pressure rise 2} pressure 9.999999999999993 sensor coffee pump pressure exit_type pressure_over exit_flow_over 6 exit_pressure_over 7.00 max_flow_or_pressure 0.0 seconds 15.00 exit_pressure_under 0} {exit_if 0 flow 6.50 volume 500 max_flow_or_pressure_range 0.6 transition fast exit_flow_under 0 temperature 85.00 name {Pause 2} pressure -3.885780586188048e-16 sensor coffee pump pressure exit_type pressure_over exit_flow_over 6 exit_pressure_over 7.00 max_flow_or_pressure 0 seconds 15.00 exit_pressure_under 0} {exit_if 0 flow 6.50 volume 500 max_flow_or_pressure_range 0.6 transition fast exit_flow_under 0 temperature 85.00 name {Pressure rise 3} pressure 9.999999999999993 sensor coffee pump pressure exit_type pressure_over exit_flow_over 6 exit_pressure_over 7.00 max_flow_or_pressure 0 seconds 15.00 exit_pressure_under 0} {exit_if 0 flow 6.50 volume 500 max_flow_or_pressure_range 0.6 transition fast exit_flow_under 0 temperature 85.00 name {Pause 3} pressure -5.717648576819556e-15 sensor coffee pump pressure exit_type pressure_over exit_flow_over 6 exit_pressure_over 7.00 max_flow_or_pressure 0 seconds 15.00 exit_pressure_under 0} {exit_if 0 flow 6.50 volume 500 max_flow_or_pressure_range 0.6 transition fast exit_flow_under 0 temperature 85.00 name {Pressure rise 4} pressure 9.999999999999993 sensor coffee pump pressure exit_type pressure_over exit_flow_over 6 exit_pressure_over 7.00 max_flow_or_pressure 0 seconds 15.00 exit_pressure_under 0} {exit_if 0 flow 6.50 volume 500 max_flow_or_pressure_range 0.6 transition fast exit_flow_under 0 temperature 85.00 name {Pause 4} pressure -5.717648576819556e-15 sensor coffee pump pressure exit_type pressure_over exit_flow_over 6 exit_pressure_over 7.00 max_flow_or_pressure 0 seconds 15.00 exit_pressure_under 0} {exit_if 0 flow 6.50 volume 500 max_flow_or_pressure_range 0.6 transition fast exit_flow_under 0 temperature 85.00 name {Pressure rise 5} pressure 9.999999999999993 sensor coffee pump pressure exit_type pressure_over exit_flow_over 6 exit_pressure_over 7.00 max_flow_or_pressure 0 seconds 15.00 exit_pressure_under 0}}
 espresso_temperature_steps_enabled 0
-final_desired_shot_volume 36
-final_desired_shot_volume_advanced 36
-final_desired_shot_volume_advanced_count_start 2
-final_desired_shot_weight 36
-final_desired_shot_weight_advanced 36
-flow_profile_decline 1.0
-flow_profile_decline_time 23
-flow_profile_hold 4.0
-flow_profile_hold_time 2
-flow_profile_minimum_pressure 6
+author Decent
+espresso_hold_time 10
+preinfusion_time 20
+espresso_pressure 8.4
+espresso_decline_time 30
+pressure_end 6.0
+espresso_temperature 85.0
+espresso_temperature_0 85.0
+espresso_temperature_1 85.0
+espresso_temperature_2 85.0
+espresso_temperature_3 85.0
+settings_profile_type settings_2c
 flow_profile_preinfusion 4.2
 flow_profile_preinfusion_time 6
-maximum_flow 0
-maximum_flow_range 0.6
-maximum_flow_range_advanced 0.6
-maximum_pressure 0
-maximum_pressure_range 0.6
-maximum_pressure_range_advanced 0.6
+flow_profile_hold 4.0
+flow_profile_hold_time 2
+flow_profile_decline 1.0
+flow_profile_decline_time 23
+flow_profile_minimum_pressure 6
 preinfusion_flow_rate 3.5
-preinfusion_stop_pressure 4.0
-preinfusion_time 20
-pressure_end 6.0
-profile_hide 1
-profile_language kr
 profile_notes {A convenient way to 5 times flush and clean your group head.  (1) Put a blind basket in your portafilter  (2) Put 3g (1/2 teaspoon) of espresso machine detergent in the blind basket  (3) Run this profile  (4) When it is done, remove the portafilter off Flush the DE1 until the water goes clear  (5) Put the portafilter back without detergent and run this profile again  (6) Finally, run FLUSH for 20 seconds.}
-profile_title {Cleaning/Forward Flush x5}
-settings_profile_type settings_2c
+final_desired_shot_volume 0.0
+final_desired_shot_weight 0.0
+final_desired_shot_weight_advanced 0.0
 tank_desired_water_temperature 0
-water_temperature 76
-
+final_desired_shot_volume_advanced 0
+profile_title {Cleaning/Forward Flush x5}
+profile_language kr
+preinfusion_stop_pressure 4.0
+profile_hide 0
+final_desired_shot_volume_advanced_count_start 2
+beverage_type Cleaning
+maximum_pressure 0
+maximum_pressure_range_advanced 0.6
+maximum_flow_range_advanced 0.6
+maximum_flow 0
+maximum_pressure_range_default 0.9
+maximum_flow_range_default 1.0
