d2be7d576940bd29276b57e82eabec444ff7980a
John Buckman <john@decentespresso.com>
2022-02-12 11:11:39 -0800

    Hide "tea in a basket" new profile, by default

--- de1plus/profiles/tea_in_a_basket.tcl
+++ de1plus/profiles/tea_in_a_basket.tcl
@@ -1,41 +1,41 @@
 advanced_shot {{exit_if 0 flow 4 volume 0 max_flow_or_pressure_range 0.6 transition fast exit_flow_under 0 temperature 100.00 name {preinfusion boost} pressure 1 sensor water pump flow exit_type pressure_over exit_flow_over 0 exit_pressure_over 4 max_flow_or_pressure 0 exit_pressure_under 0 seconds 4.00} {exit_if 0 flow 0.5 volume 0 max_flow_or_pressure_range 0.6 transition fast exit_flow_under 0 temperature 100.00 name flow sensor water pump flow exit_flow_over 6 exit_pressure_over 0 max_flow_or_pressure 0 seconds 5.00 exit_pressure_under 0} {exit_if 0 flow 0.0 volume 0 max_flow_or_pressure_range 0.6 transition fast exit_flow_under 0 temperature 100.00 name hold sensor water pump flow exit_flow_over 6 exit_pressure_over 0 max_flow_or_pressure 0 seconds 10.00 exit_pressure_under 0} {exit_if 0 flow 3.0 volume 0 max_flow_or_pressure_range 0.6 transition fast exit_flow_under 0 temperature 100.00 name flow sensor water pump flow exit_flow_over 6 exit_pressure_over 0 max_flow_or_pressure 0 seconds 12.00 exit_pressure_under 0} {exit_if 0 flow 0 volume 0 max_flow_or_pressure_range 0.6 transition fast exit_flow_under 0 temperature 100.00 name hold sensor water pump flow exit_flow_over 6 exit_pressure_over 0 max_flow_or_pressure 0 seconds 12.00 exit_pressure_under 0} {exit_if 0 flow 3.0 volume 0 max_flow_or_pressure_range 0.6 transition fast exit_flow_under 0 temperature 100.00 name flow sensor water pump flow exit_flow_over 6 max_flow_or_pressure 0 exit_pressure_over 0 exit_pressure_under 0 seconds 12.00} {exit_if 0 flow 0 volume 0 max_flow_or_pressure_range 0.6 transition fast exit_flow_under 0 temperature 100.00 name hold sensor water pump flow exit_flow_over 6 exit_pressure_over 0 max_flow_or_pressure 0 seconds 12.00 exit_pressure_under 0} {exit_if 0 flow 3.0 volume 0 max_flow_or_pressure_range 0.6 transition fast exit_flow_under 0 temperature 100.00 name flow sensor water pump flow exit_flow_over 6 max_flow_or_pressure 0 exit_pressure_over 0 exit_pressure_under 0 seconds 12.00} {exit_if 0 flow 0 volume 0 max_flow_or_pressure_range 0.6 transition fast exit_flow_under 0 temperature 100.00 name hold sensor water pump flow exit_flow_over 6 exit_pressure_over 0 max_flow_or_pressure 0 seconds 12.00 exit_pressure_under 0} {exit_if 0 flow 3.0 volume 0 max_flow_or_pressure_range 0.6 transition fast exit_flow_under 0 temperature 100.00 name flow sensor water pump flow exit_flow_over 6 max_flow_or_pressure 0 exit_pressure_over 0 exit_pressure_under 0 seconds 12.00} {exit_if 0 flow 0.1 volume 0 max_flow_or_pressure_range 0.6 transition fast exit_flow_under 0 temperature 100.00 name hold sensor water pump flow exit_flow_over 6 exit_pressure_over 0 max_flow_or_pressure 0 seconds 12.00 exit_pressure_under 0} {exit_if 0 flow 3.0 volume 0 max_flow_or_pressure_range 0.6 transition fast exit_flow_under 0 temperature 100.00 name flow sensor water pump flow exit_flow_over 6 exit_pressure_over 0 max_flow_or_pressure 0 seconds 12.00 exit_pressure_under 0} {exit_if 0 flow 0.0 volume 0 max_flow_or_pressure_range 0.6 transition fast exit_flow_under 0 temperature 100.00 name hold sensor water pump flow exit_flow_over 6 exit_pressure_over 0 max_flow_or_pressure 0 seconds 10.00 exit_pressure_under 0} {exit_if 0 flow 3.0 volume 0 max_flow_or_pressure_range 0.6 transition fast exit_flow_under 0 temperature 100.00 name flow sensor water pump flow exit_flow_over 6 exit_pressure_over 0 max_flow_or_pressure 0 seconds 10.00 exit_pressure_under 0} {exit_if 0 flow 0 volume 0 max_flow_or_pressure_range 0.6 transition fast exit_flow_under 0 temperature 100.00 name flow sensor water pump flow exit_flow_over 6 exit_pressure_over 0 max_flow_or_pressure 0 seconds 10.00 exit_pressure_under 0} {exit_if 0 flow 3.0 volume 0 max_flow_or_pressure_range 0.6 transition fast exit_flow_under 0 temperature 100.00 name flow sensor water pump flow exit_flow_over 6 exit_pressure_over 0 max_flow_or_pressure 0 seconds 10.00 exit_pressure_under 0}}
-espresso_temperature_steps_enabled 1
 author Decent
-espresso_hold_time 25
-preinfusion_time 0
-espresso_pressure 8.6
+beverage_type tea
 espresso_decline_time 0
-pressure_end 6.0
+espresso_hold_time 25
+espresso_pressure 8.6
 espresso_temperature 100.00
 espresso_temperature_0 90.0
 espresso_temperature_1 88.0
 espresso_temperature_2 88.0
 espresso_temperature_3 88.0
-settings_profile_type settings_2c
-flow_profile_preinfusion 4
-flow_profile_preinfusion_time 5
-flow_profile_hold 2
-flow_profile_hold_time 8
-flow_profile_decline 1.2
-flow_profile_decline_time 17
-flow_profile_minimum_pressure 4
-preinfusion_flow_rate 4
-profile_notes {This profile makes an acceptable mug of tea using a regular portafilter espresso basket.  Place half a teaspoon of loose leaf tea, or a teabag, into a basket of any size.  Works best of teas that prefer high brewing temperatures, such as English Breakfast.  For better quality brews, the tea portafilter, but this technique works well enough if you don't own that.}
+espresso_temperature_steps_enabled 1
 final_desired_shot_volume 36
+final_desired_shot_volume_advanced 295
+final_desired_shot_volume_advanced_count_start 0
 final_desired_shot_weight 100
 final_desired_shot_weight_advanced 350
-tank_desired_water_temperature 0
-final_desired_shot_volume_advanced 295
-profile_title {Tea/in a basket}
-profile_language en
-preinfusion_stop_pressure 4
-profile_hide 0
-final_desired_shot_volume_advanced_count_start 0
-beverage_type tea_portafilter
+flow_profile_decline 1.2
+flow_profile_decline_time 17
+flow_profile_hold 2
+flow_profile_hold_time 8
+flow_profile_minimum_pressure 4
+flow_profile_preinfusion 4
+flow_profile_preinfusion_time 5
+maximum_flow 0
+maximum_flow_range_advanced 1.0
+maximum_flow_range_default 1.0
 maximum_pressure 0
 maximum_pressure_range_advanced 0.9
-maximum_flow_range_advanced 1.0
-maximum_flow 0
 maximum_pressure_range_default 0.9
-maximum_flow_range_default 1.0
+preinfusion_flow_rate 4
+preinfusion_stop_pressure 4
+preinfusion_time 0
+pressure_end 6.0
+profile_hide 1
+profile_language en
+profile_notes {This profile makes an acceptable mug of tea using a regular portafilter espresso basket.  Place half a teaspoon of loose leaf tea, or a teabag, into a basket of any size.  Works best of teas that prefer high brewing temperatures, such as English Breakfast.  For better quality brews, the tea portafilter, but this technique works well enough if you don't own that.}
+profile_title {Tea/in a basket}
+settings_profile_type settings_2c
+tank_desired_water_temperature 0
 
