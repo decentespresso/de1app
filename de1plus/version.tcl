a1a9c1bcbacd1de1fd72305970500e8799cd2377
Mimoja <git@mimoja.de>
2022-02-26 21:17:09 +0100

    Update Changelog link for v1.39
    Signed-off-by: Mimoja <git@mimoja.de>

--- de1plus/version.tcl
+++ de1plus/version.tcl
@@ -1,4 +1,4 @@
 package ifneeded de1app 99.99.99 {}
 # The package version is set by the build script. This is just a dummy value
 
-set ::changelog_link "https://public.3.basecamp.com/p/GQVVtDpa7uKL7tUcp6KDe8Ma"
+set ::changelog_link "https://public.3.basecamp.com/p/avbBZux98SnD5YaCdSTBhSzB"
