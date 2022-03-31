ed6713fa5d3a55ce2810c0a7bc4eabbed217a03c
John Buckman <john@decentespresso.com>
2022-03-30 18:54:13 -0700

    new Basecamp changelog link, that is more concise, just has "best of"

--- de1plus/version.tcl
+++ de1plus/version.tcl
@@ -1,4 +1,4 @@
 package ifneeded de1app 99.99.99 {}
 # The package version is set by the build script. This is just a dummy value
 
-set ::changelog_link "https://public.3.basecamp.com/p/avbBZux98SnD5YaCdSTBhSzB"
+set ::changelog_link "https://public.3.basecamp.com/p/jr68WyniVmfAfr5Q5vZWYfK7"
