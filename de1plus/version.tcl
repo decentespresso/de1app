b0538be3ae5338c7755c074dafd7decae30187ab
John Buckman <john@decentespresso.com>
2022-09-11 13:06:14 +0100

    update html page reference to describe this new app version

--- de1plus/version.tcl
+++ de1plus/version.tcl
@@ -1,4 +1,4 @@
 package ifneeded de1app 99.99.99 {}
 # The package version is set by the build script. This is just a dummy value
 
-set ::changelog_link "https://public.3.basecamp.com/p/jr68WyniVmfAfr5Q5vZWYfK7"
+set ::changelog_link "https://decentespresso.com/new-in-v144.html"
