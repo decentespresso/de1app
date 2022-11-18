7181726dfb6d9a8d398e46af599b49ec4147ebc9
John Buckman <john@decentespresso.com>
2022-11-18 13:55:41 +0800

    handle without error, case where Acaia scale sends invalid or unrecognized data

--- de1plus/bluetooth.tcl
+++ de1plus/bluetooth.tcl
@@ -710,18 +710,22 @@ proc acaia_find_payload {event_t} {
 }
 
 proc acaia_decode_weight {payload_offset} {
-	set weight_payload [lrange $::acaia_command_buffer $payload_offset end]
-	set value [expr {(([lindex $weight_payload 1] & 0xFF) << 8) + ([lindex $weight_payload 0] & 0xFF)}]
-	set unit [expr {[lindex $weight_payload 4] & 0xFF}];
-	set calculated_weight [expr { $value / pow(10.0, $unit)}]
-	set is_negative [expr {[lindex $weight_payload 5] > 1.0}]
-	if {$is_negative} {
-		set calculated_weight [expr {$calculated_weight * -1.0}]
-	}
+	set value 0
+	catch {
+		# wrap everything in a catch in case bad data is sent by Acaia
+		set weight_payload [lrange $::acaia_command_buffer $payload_offset end]
+		set value [expr {(([lindex $weight_payload 1] & 0xFF) << 8) + ([lindex $weight_payload 0] & 0xFF)}]
+		set unit [expr {[lindex $weight_payload 4] & 0xFF}];
+		set calculated_weight [expr { $value / pow(10.0, $unit)}]
+		set is_negative [expr {[lindex $weight_payload 5] > 1.0}]
+		if {$is_negative} {
+			set calculated_weight [expr {$calculated_weight * -1.0}]
+		}
 	# msg -DEBUG "WEIGHT $calculated_weight \
 		# UNIT $unit IS_NEG $is_negative BUFFER $::acaia_command_buffer"
-	set sensorweight $calculated_weight
-	::device::scale::process_weight_update $sensorweight ;# $event_time
+		set sensorweight $calculated_weight
+		::device::scale::process_weight_update $sensorweight ;# $event_time
+	}
 }
 
 #### Decent Scale
