fd05572148c771ac2d00dac974c20e19029812c9
John Buckman <john@decentespresso.com>
2024-03-01 18:40:22 +0000

    Updated help URL for info about v1.43 version

--- de1plus/version.tcl
+++ de1plus/version.tcl
@@ -1,4 +1,4 @@
 package ifneeded de1app 99.99.99 {}
 # The package version is set by the build script. This is just a dummy value
 
-set ::changelog_link "https://decentespresso.com/de1app-v142.html"
+set ::changelog_link "https://decentespresso.com/de1app-v143.html"
