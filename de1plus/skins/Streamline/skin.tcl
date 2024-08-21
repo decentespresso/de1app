7da264099afc749082147cf6589dc7ce59a26dc5
John Buckman <john@decentespresso.com>
2024-08-21 17:45:24 +0200

    Streamline: remove long-press-on-settings-button-to-exit as it seems to falsely fire at times.

--- de1plus/skins/Streamline/skin.tcl
+++ de1plus/skins/Streamline/skin.tcl
@@ -2126,7 +2126,9 @@ set right_buttons_width 200
 set right_buttons_separation 20
 
 dui add dbutton $::all_pages [expr {$right_buttons_start - (2*$right_buttons_width) - (2*$right_buttons_separation)}] 66 [expr {$right_buttons_start - $right_buttons_width - $right_buttons_separation}] 155 -tags settings_btn -label [translate "Settings"]  -command { say [translate {settings}] $::settings(sound_button_out); show_settings "" "back_from_settings" }
-dui add dbutton $::all_pages [expr {$right_buttons_start - $right_buttons_width}] 66 $right_buttons_start 155 -tags sleep_btn -label [translate "Sleep"]  -command { say [translate {sleep}] $::settings(sound_button_out); start_sleep } -longpress_threshold $::streamline_longpress_threshold -longpress_cmd { say [translate {Edit}] $::settings(sound_button_out); streamline_app_exit_button } 
+dui add dbutton $::all_pages [expr {$right_buttons_start - $right_buttons_width}] 66 $right_buttons_start 155 -tags sleep_btn -label [translate "Sleep"]  -command { say [translate {sleep}] $::settings(sound_button_out); start_sleep } 
+#-longpress_threshold $::streamline_longpress_threshold 
+#-longpress_cmd { say [translate {Exit}] $::settings(sound_button_out); streamline_app_exit_button } 
 
 proc streamline_app_exit_button {} {
 
@@ -2619,13 +2621,14 @@ proc streamline_profile_select { slot } {
 	set selected_profile [dict get $profiles $slot name]
 	if {$selected_profile == ""} {
 		# empty button
+		# john source of a potential bug where a button is cleared, need to investigate further 8/11/2024
 		set ::streamline_double_tap_empty_preset_button_previous_profile_filename $::settings(profile_filename)
 		set ::streamline_double_tap_empty_preset_button_slot $slot
 		show_settings "settings_1" "store_profile_choice_to_profile_preset_button"
 		return
 	}
 
-	puts "ERROR select profile '[dict get $profiles $slot name]'"
+	#puts "ERROR select profile '[dict get $profiles $slot name]'"
 
 	set result [select_profile $selected_profile]
 	if {$result == "-1"} {
