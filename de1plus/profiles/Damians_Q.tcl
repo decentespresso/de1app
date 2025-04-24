d8c365fca92f93df72c03c1eb154be18df0fc1e8
John Buckman <john@decentespresso.com>
2025-04-24 10:35:10 +0800

    Added two new profiles: Damian's "Q" (best for medium-light roasts) and John Weiss' "Advanced Spring Lever"

---
+++ de1plus/profiles/Damians_Q.tcl
@@ -0,0 +1,43 @@
+advanced_shot {{exit_if 1 flow 8 volume 60.00 max_flow_or_pressure_range 0.2 transition fast exit_flow_under 0 temperature 84.0 weight 0.00 name Filling pressure 6.0 pump pressure sensor coffee exit_type pressure_over exit_flow_over 6 exit_pressure_over 3.0 max_flow_or_pressure 0 exit_pressure_under 0 seconds 25.00} {exit_if 0 flow 8 volume 100.00 max_flow_or_pressure_range 0.2 transition fast exit_flow_under 0 temperature 94.0 weight 0.0 name Infusing pressure 6.0 pump pressure sensor coffee exit_type pressure_over exit_flow_over 6 exit_pressure_over 3.0 max_flow_or_pressure 0 exit_pressure_under 0 seconds 1.0} {exit_if 0 flow 1.8 volume 100 max_flow_or_pressure_range 0.2 transition fast exit_flow_under 0 temperature 94.0 weight 0.00 name Pouring pressure 4.8 pump flow sensor coffee exit_type flow_over exit_flow_over 2.80 exit_pressure_over 11 max_flow_or_pressure 10.0 exit_pressure_under 0 seconds 127}}
+author Damian
+beverage_type espresso
+espresso_decline_time 30
+espresso_hold_time 16
+espresso_pressure 6.0
+espresso_temperature 84.0
+espresso_temperature_0 84.0
+espresso_temperature_1 84.0
+espresso_temperature_2 84.0
+espresso_temperature_3 84.0
+espresso_temperature_steps_enabled 0
+final_desired_shot_volume 36
+final_desired_shot_volume_advanced 0
+final_desired_shot_volume_advanced_count_start 2
+final_desired_shot_weight 50
+final_desired_shot_weight_advanced 36
+flow_profile_decline 1.2
+flow_profile_decline_time 17
+flow_profile_hold 2
+flow_profile_hold_time 8
+flow_profile_minimum_pressure 4
+flow_profile_preinfusion 4
+flow_profile_preinfusion_time 5
+maximum_flow 0
+maximum_flow_range_advanced 0.2
+maximum_flow_range_default 1.0
+maximum_pressure 0
+maximum_pressure_range_advanced 0.2
+maximum_pressure_range_default 0.9
+preinfusion_flow_rate 4
+preinfusion_stop_pressure 4.0
+preinfusion_time 20
+pressure_end 4.0
+profile_hide 1
+profile_language en
+profile_notes {A very popular profile made with D-Flow, spun out as its own profile.  Optimized for medium-light beans. By Damian Brakel https://www.diy.brakel.com.au/}
+profile_title {Damian's Q}
+read_only 1
+read_only_backup {}
+settings_profile_type settings_2c
+tank_desired_water_temperature 0
+
