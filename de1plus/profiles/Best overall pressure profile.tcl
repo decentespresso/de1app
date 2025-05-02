569071e5ddbb4fba166b20c04324f64c7b5b0aad
John Buckman <john@decentespresso.com>
2025-05-02 18:39:36 +0800

    Built in profiles reset to "read only" -- they were accidentally set to not read only, in a recent update.

--- de1plus/profiles/Best overall pressure profile.tcl
+++ de1plus/profiles/Best overall pressure profile.tcl
@@ -1,46 +1,6 @@
 advanced_shot {}
 author Decent
-read_only 0
-read_only_backup {advanced_shot {}
-author Decent
-espresso_hold_time 10
-preinfusion_time 20
-espresso_pressure 8.4
-espresso_decline_time 30
-pressure_end 6.0
-espresso_temperature 88.0
-espresso_temperature_0 88.0
-espresso_temperature_1 88.0
-espresso_temperature_2 88.0
-espresso_temperature_3 88.0
-settings_profile_type settings_2a
-flow_profile_preinfusion 4.2
-flow_profile_preinfusion_time 6
-flow_profile_hold 4.0
-flow_profile_hold_time 2
-flow_profile_decline 1
-flow_profile_decline_time 23
-flow_profile_minimum_pressure 6
-preinfusion_flow_rate 3.5
-profile_notes {We recommend this pressure profile as the most likely to produce a good espresso in the most varied number of cases.  The decreasing pressure will help reduce acidity.}
-final_desired_shot_volume 36
-final_desired_shot_weight 36
-final_desired_shot_weight_advanced 36
-tank_desired_water_temperature 0
-final_desired_shot_volume_advanced 0
-profile_title {Best overall pressure profile}
-profile_language en
-preinfusion_stop_pressure 4
-profile_hide 0
-final_desired_shot_volume_advanced_count_start 0
-beverage_type espresso
-maximum_pressure 0
-maximum_pressure_range_advanced 0.6
-maximum_flow_range_advanced 0.6
-maximum_flow 3.5
-maximum_pressure_range_default 0.9
-maximum_flow_range_default 1.0
-read_only 1}
+read_only 1
 espresso_hold_time 10
 preinfusion_time 20
 espresso_pressure 8.4
--- de1plus/profiles/e61 rocketing up to 10 bar.tcl
+++ de1plus/profiles/e61 rocketing up to 10 bar.tcl
@@ -35,39 +35,7 @@ profile_hide 1
 profile_language en
 profile_notes {With a technician's help, you could sometimes boost the pressure on the classic E61 machine to 10 bar.  You will need to grind finer, and your espresso will be very thick.  Excellent for medium roasted beans mixed with whole milk.}
 profile_title {E61 rocketing up to 10 bar}
-read_only 0
-read_only_backup {advanced_shot {}
-author Decent
-beverage_type espresso
-espresso_decline_time 0
-espresso_hold_time 30
-espresso_pressure 10.0
-espresso_temperature 92.0
-final_desired_shot_volume 36
-final_desired_shot_volume_advanced 0
-final_desired_shot_weight 36
-final_desired_shot_weight_advanced 36
-flow_profile_decline 1
-flow_profile_decline_time 23
-flow_profile_hold 1.8
-flow_profile_hold_time 0
-flow_profile_minimum_pressure 6
-flow_profile_preinfusion 4.2
-flow_profile_preinfusion_time 6
-preinfusion_flow_rate 4.5
-preinfusion_guarantee 1
-preinfusion_stop_pressure 4
-preinfusion_time 10
-pressure_end 10.0
-profile_hide 0
-profile_language en
-profile_notes {With a technician's help, you could sometimes boost the pressure on the classic E61 machine to 10 bar.  You will need to grind finer, and your espresso will be very thick.  Excellent for medium roasted beans mixed with whole milk.}
-profile_title {E61 rocketing up to 10 bar}
 read_only 1
-settings_profile_type settings_2a
-tank_desired_water_temperature 0
-
-}
 settings_profile_type settings_2a
 tank_desired_water_temperature 0
 
