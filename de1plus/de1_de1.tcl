decentespresso/de1app

commit 8cc6cf6fa82dd4c5cfffd8004ceaabec09659bc3

SAV is disabled if a scale is connected, and SAW is then used.

de1plus/de1_de1.tcl
@@ -789,7 +789,7 @@ namespace eval ::de1::sav {
 
 		if { $state_text == "None" } { set state_text $::de1_num_state($::de1(state)) }
 
-		expr { $state_text in {{Espresso} {HotWater}} }
+		expr { $state_text in {{Espresso} {HotWater}} && $::de1(scale_device_handle) == 0 }
 	}
 
 	proc is_active {} {

de1plus/machine.tcl
@@ -39,20 +39,20 @@ array set ::de1 {
 	factory_calibration_flow {}
 	scale_max_connection_retry_attempts 20
 	advanced_shot_moveone_enabled 1
-    found    0
-    decentscale_timer_on 0
-    bluetooth_scale_connection_attempts_tried 0
-    battery_discharging 0
-    refill_kit_detected {}
-    scanning 1
-    usb_charger_on 1
-    device_handle 0
-    scale_battery_level 100
-    scale_fw_version 0
-    scale_usb_powered 0
-    language_rtl 0
-    scale_device_handle 0
-    decentscale_device_handle 0
+  found    0
+  decentscale_timer_on 0
+  bluetooth_scale_connection_attempts_tried 0
+  battery_discharging 0
+  refill_kit_detected {}
+  scanning 1
+  usb_charger_on 1
+  device_handle 0
+  scale_battery_level 100
+  scale_fw_version 0
+  scale_usb_powered 0
+  language_rtl 0
+  scale_device_handle 0
+  decentscale_device_handle 0
 	suuid "0000A000-0000-1000-8000-00805F9B34FB"
 	sinstance 12
 	cuuid "0000A002-0000-1000-8000-00805F9B34FB"

commit f67c0c68b6e6d04b1a31abd1cf2a3596931491ca

Fixed bug caused by https://github.com/decentespresso/de1app/commit/8cc6cf6fa82dd4c5cfffd8004ceaabec09659bc3 that made water volume not be tracked in a scale was connected.

de1plus/de1_de1.tcl
@@ -789,16 +789,21 @@ namespace eval ::de1::sav {
 
 		if { $state_text == "None" } { set state_text $::de1_num_state($::de1(state)) }
 
-		expr { $state_text in {{Espresso} {HotWater}} && $::de1(scale_device_handle) == 0 }
+		expr { $state_text in {{Espresso} {HotWater}}  }
 	}
 
 	proc is_active {} {
+
 
 		expr {$::de1::sav::_is_active_flag}
 	}
 
 
 	proc start_active {} {
+
+		if {$::de1(scale_device_handle) != 0} {
+			msg -NOTICE "::de1::sav::start_active: disabled SAV because scale is connected"
+		}
 
 		if { [::de1::sav::is_active] } {
 			msg -NOTICE "::de1::sav::start_active: already active"